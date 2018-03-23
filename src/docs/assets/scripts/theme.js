46449b0efe1c9ac25e2e878b4ab536630ff58ed7
Riccardo Caroli <rcaroli@webgriffe.com>
2018-03-23 12:20:04 +0100

    header and footer doc

--- src/docs/assets/scripts/theme.js
+++ src/docs/assets/scripts/theme.js
@@ -5,3 +5,14 @@
 import '../../../scripts/xtend.js';
 
 console.log('theme loaded');
+
+// @TODO toggle
+
+document.getElementsByClassName('btn-menu')[0].addEventListener('click', function (e) {
+  console.log(e);
+  if (this.classList.contains('active')) {
+    this.classList.remove('active');
+  } else {
+    this.classList.add('active');
+  }
+});
