cf6ba98c176076cecb4a371d994126a9e8ed8490
Riccardo Caroli <rcaroli@webgriffe.com>
2018-04-19 12:19:38 +0200

    v0.3.1

--- dist/xtend.js
+++ dist/xtend.js
@@ -1,5 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-/*! xtend v0.3.0 (https://getxtend.com/)
+/*! xtend v0.3.1 (https://getxtend.com/)
 @copyright (c) 2017 - 2018 Riccardo Caroli
 @license MIT (https://github.com/minimit/xtend-library/blob/master/LICENSE) */
 
@@ -429,7 +429,7 @@ setTimeout(function () {
 */
 
 },{"./xtend":2}],2:[function(require,module,exports){
-/*! xtend v0.3.0 (https://getxtend.com/)
+/*! xtend v0.3.1 (https://getxtend.com/)
 @copyright (c) 2017 - 2018 Riccardo Caroli
 @license MIT (https://github.com/minimit/xtend-library/blob/master/LICENSE) */
 
