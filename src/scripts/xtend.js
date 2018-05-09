cc0721a9b500fe81cc3f0d81f466ace5577e364d
Riccardo Caroli <rcaroli@webgriffe.com>
2018-05-09 13:29:15 +0200

    0.4.5

--- src/scripts/xtend.js
+++ src/scripts/xtend.js
@@ -1,4 +1,4 @@
-/*! xtend v0.4.4 (https://getxtend.com/)
+/*! xtend v0.4.5 (https://getxtend.com/)
 @copyright (c) 2017 - 2018 Riccardo Caroli
 @license MIT (https://github.com/minimit/xtend-library/blob/master/LICENSE) */
 
