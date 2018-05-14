d10dcef2c867020cb63bd0d0f5c8327853100e77
Riccardo Caroli <rcaroli@webgriffe.com>
2018-05-14 17:31:02 +0200

    copyright version

--- src/scripts/xtend-utils.js
+++ src/scripts/xtend-utils.js
@@ -1,3 +1,7 @@
+/*! xtend (https://getxtend.com/)
+@copyright (c) 2017 - 2018 Riccardo Caroli
+@license MIT (https://github.com/minimit/xtend-library/blob/master/LICENSE) */
+
 'use strict';
 
 import {XtToggle, XtDrop, XtOverlay, XtSticky} from './xtend';
--- src/scripts/xtend.js
+++ src/scripts/xtend.js
@@ -1,4 +1,4 @@
-/*! xtend (https://getxtend.com/)
+/* xtend (https://getxtend.com/)
 @copyright (c) 2017 - 2018 Riccardo Caroli
 @license MIT (https://github.com/minimit/xtend-library/blob/master/LICENSE) */
 
