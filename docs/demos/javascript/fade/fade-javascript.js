286a56cb83cbb1b3def4b7034c00459dedd0bf61
Riccardo Caroli <rcaroli@webgriffe.com>
2018-07-26 15:20:49 +0200

    pre deploy gh-pages

--- docs/demos/javascript/fade/fade-javascript.js
+++
@@ -1,4 +0,0 @@
-
-new XtFade(document.documentElement, {
-  // options
-});
--- docs/demos/javascript/fade/fade-mode-block.js
+++
@@ -1,7 +0,0 @@
-
-setTimeout( function() {
-  for (let el of document.querySelectorAll('.fade')) {
-    el.classList.add('fade-block');
-  }
-  alert('fade blocked');
-}, 2000);
