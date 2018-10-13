5e54a237f01eb85ddb7743723bca6f1fc49bb4d2
Riccardo Caroli <rcaroli@webgriffe.com>
2018-10-13 19:32:10 +0200

    Updates

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -65,8 +65,7 @@ for(let el of document.querySelectorAll('.site-aside-text')) {
     "targets": ".site-aside-subsub",
     "on": "mouseenter",
     "off": "mouseleave",
-    "noQueue": true,
-    "noDuration": {"elements": true, "targets": true, "elementsInner": true, "targetsInner": true},
+    "durationNone": {"elements": true, "targets": true, "elementsInner": true, "targetsInner": true},
     "min": 0,
     "max": Infinity
   });
