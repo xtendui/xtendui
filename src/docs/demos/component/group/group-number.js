0849e74eb785205af48819c492e421197babddaa
Riccardo Caroli <rcaroli@webgriffe.com>
2018-10-18 10:54:19 +0200

    revision group-number

--- src/docs/demos/component/group/group-number.js
+++ src/docs/demos/component/group/group-number.js
@@ -9,8 +9,8 @@ function inputNumberValidate(val) {
   addEl.removeAttribute('disabled');
   removeEl.removeAttribute('disabled');
   // check min and max
-  let inputMin = parseFloat(input.getAttribute('data-min')) || 0;
-  let inputMax = parseFloat(input.getAttribute('data-max')) || Infinity;
+  let inputMin = parseFloat(input.getAttribute('min')) || 1;
+  let inputMax = parseFloat(input.getAttribute('max')) || Infinity;
   if (val <= inputMin) {
     val = inputMin;
     removeEl.setAttribute('disabled', 'disabled');
@@ -39,23 +39,28 @@ function inputNumberInit(container) {
   for (let [i, group] of container.querySelectorAll('.group-number-line, .group-number-stack').entries()) {
     // init
     inputNumberChange.bind(group, 0)();
+    // vars
+    let inputEl = group.querySelectorAll('input')[0];
+    let step = parseFloat(inputEl.getAttribute('step')) || 1;
     // add
-    let addStep = 1;
-    let addHandler = inputNumberChange.bind(group, addStep);
     let addEl = group.querySelectorAll('.group-number-add')[0];
-    addEl.removeEventListener('click', addHandler)
+    let addStep = step;
+    let addHandler = Xt.dataStorage.get(addEl, 'addHandler');
+    addHandler = addHandler ? addHandler : Xt.dataStorage.put(addEl, 'addHandler', inputNumberChange.bind(group, addStep));
+    addEl.removeEventListener('click', addHandler);
     addEl.addEventListener('click', addHandler);
     // remove
-    let removeStep = -1;
-    let removeHandler = inputNumberChange.bind(group, removeStep);
     let removeEl = group.querySelectorAll('.group-number-remove')[0];
+    let removeStep = -step;
+    let removeHandler = Xt.dataStorage.get(removeEl, 'removeHandler');
+    removeHandler = removeHandler ? removeHandler : Xt.dataStorage.put(removeEl, 'removeHandler', inputNumberChange.bind(group, removeStep));
     removeEl.removeEventListener('click', removeHandler);
     removeEl.addEventListener('click', removeHandler);
     // change
-    let input = group.querySelectorAll('input')[0];
-    let inputHandler = inputNumberChange.bind(group, 0);
-    input.removeEventListener('change', inputHandler);
-    input.addEventListener('change', inputHandler);
+    let inputHandler = Xt.dataStorage.get(inputEl, 'inputHandler');
+    inputHandler = inputHandler ? inputHandler : Xt.dataStorage.put(inputEl, 'inputHandler', inputNumberChange.bind(group, 0));
+    inputEl.removeEventListener('change', inputHandler);
+    inputEl.addEventListener('change', inputHandler);
   }
 }
 
