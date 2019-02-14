048b541968a22bf3b28959b27165d8a199b7b6fe
Riccardo Caroli <rcaroli@webgriffe.com>
2019-02-14 14:37:40 +0100

    fix follow mouse

--- src/docs/demos/presentation/slider/progress.js
+++ src/docs/demos/presentation/slider/progress.js
@@ -105,32 +105,42 @@ Xt.observe.push({
     let height;
 
     function mousemove(e) {
-      // position
-      let top = e.clientY + height / 2;
-      let left = e.clientX + width / 2;
-      loader.style.top = top + 'px';
-      loader.style.left = left + 'px';
+      requestAnimationFrame(function () {
+        // fix initial
+        if (width === undefined) {
+          mouseenter(e);
+        }
+        // position
+        let top = e.clientY + height / 2;
+        let left = e.clientX + width / 2;
+        loader.style.top = top + 'px';
+        loader.style.left = left + 'px';
+      });
     }
 
     function mouseenter(e) {
-      // size
-      let rect = loader.getBoundingClientRect();
-      width = rect.width;
-      height = rect.height;
-      // class
-      loader.classList.add('active');
-      loader.classList.remove('out');
-      // position
-      let top = e.clientY + height / 2;
-      let left = e.clientX + width / 2;
-      loader.style.top = top + 'px';
-      loader.style.left = left + 'px';
+      requestAnimationFrame(function () {
+        // size
+        let rect = loader.getBoundingClientRect();
+        width = rect.width;
+        height = rect.height;
+        // class
+        loader.classList.add('active');
+        loader.classList.remove('out');
+        // position
+        let top = e.clientY + height / 2;
+        let left = e.clientX + width / 2;
+        loader.style.top = top + 'px';
+        loader.style.left = left + 'px';
+      });
     }
 
     function mouseleave(e) {
-      // class
-      loader.classList.remove('active');
-      loader.classList.add('out');
+      requestAnimationFrame(function () {
+        // class
+        loader.classList.remove('active');
+        loader.classList.add('out');
+      });
     }
 
     container.removeEventListener('mousemove', mousemove);
