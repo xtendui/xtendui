32f57860e0e240660a592b8b9bb6b3e7bf35f7d2
Riccardo Caroli <rcaroli@webgriffe.com>
2019-02-16 17:42:05 +0100

    Updates

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -1,7 +1,3 @@
-/* xtend (https://getxtend.com/)
-@copyright (c) 2017 - 2018 Riccardo Caroli
-@license MIT (https://github.com/minimit/xtend-library/blob/master/LICENSE) */
-
 //////////////////////
 // import
 //////////////////////
