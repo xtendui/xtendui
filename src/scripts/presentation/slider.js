5e4867e5e6c9949bc93c144fe47111872df20c0f
Riccardo Caroli <rcaroli@webgriffe.com>
2019-04-05 18:40:02 +0200

    chore(slider): removed initSliderPos on each on

--- src/scripts/presentation/slider.js
+++ src/scripts/presentation/slider.js
@@ -463,10 +463,6 @@ class Slider extends Core {
     for (let target of targets) {
       Xt.dataStorage.put(target, self.componentNamespace + 'SlideOnDone', true);
     }
-    // initSliderPos
-    if (self.dragger) {
-      self.initSliderPos();
-    }
     // autoHeight
     if (self.autoHeight) {
       self.eventAutoHeight(slide);
