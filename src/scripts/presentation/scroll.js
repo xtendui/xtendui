74196ed05841fbe4fbbab32e0ca5b3dd387954d3
Riccardo Caroli <beaver82@gmail.com>
2019-06-28 09:00:36 +0200

    chore(packaging): latest

--- src/scripts/presentation/scroll.js
+++ src/scripts/presentation/scroll.js
@@ -52,10 +52,10 @@ class Scroll extends Core {
           target = el.cloneNode(true);
           target.classList.add('xt-clone', 'xt-ignore');
           for (let elId of target.querySelectorAll('[id]')) {
-            elId.removeAttribute('id');
+            elId.setAttribute('id', elId.getAttribute('id') + '-clone');
           }
           for (let elName of target.querySelectorAll('[name]')) {
-            elName.removeAttribute('name');
+            elName.setAttribute('name', elName.getAttribute('name') + '-clone');
           }
           container.append(target);
         }
