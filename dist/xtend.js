2ee0a174071a321476fef35417bd383f97817b38
Riccardo Caroli <beaver82@gmail.com>
2019-08-26 12:36:20 +0200

    chore(packaging): revision polyfills

--- dist/xtend.js
+++ dist/xtend.js
@@ -2,8 +2,20 @@
 @copyright (c) 2017 - 2019 Riccardo Caroli
 @license MIT (https://github.com/minimit/xtend-library/blob/master/LICENSE) */
 
-import 'core-js'
-import 'regenerator-runtime/runtime'
+// support explorer 15, firefox 48, opera 34, safari 10, chrome 47
+
+// support old browsers ie11+
+// ADD
+// .browserslistrc ie >= 11
+// import 'xtend-library/src/polyfill-ie11.js'
+
+// POLYFILLS
+// ADD
+// https://cdnjs.cloudflare.com/ajax/libs/core-js/2.6.9/shim.min.js
+// OR
+// import 'core-js'
+// import 'regenerator-runtime/runtime'
+
 import {Xt} from 'xtend-library'
 import 'xtend-library/src/xtend-core.js'
 
--- src/xt.js
+++ src/xt.js
@@ -1,5 +1,4 @@
-import 'xtend-library/src/polyfill.js' // support explorer 15, firefox 48, opera 34, safari 10, chrome 47
-//import 'xtend-library/src/polyfill-ie11.js' // support old browsers ie11+
+import 'xtend-library/src/polyfill.js'
 
 //////////////////////
 // constructor
