f7dcde15b25acf6b8b61832da5e0fc0ce1785cdd
Riccardo Caroli <beaver82@gmail.com>
2019-08-31 14:55:36 +0200

    chore: standardjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,8 @@
 module.exports = {
-  extends: 'airbnb-base',
+  extends: 'standard',
   env: {
     browser: true,
     node: true,
     es6: true,
-  },
-  rules: {
-    'no-floating-decimal': 'off',
-    'prefer-template': 'off',
-    'no-unused-vars': 'off',
-    'object-curly-spacing': ['error', 'never'],
-    'object-curly-newline': ['error', 'never'],
-    'no-param-reassign': ['error', { 'props': false }],
-    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
-    'object-shorthand': ['error', 'always', {
-      avoidQuotes: true,
-      avoidExplicitReturnArrows: true,
-    }],
-    'function-paren-newline': ['error', 'consistent'],
-    'max-len': ['warn', 120, 2, {
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-    }],
   },
 };
