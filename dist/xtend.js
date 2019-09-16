2b59c5b8fc9c067b84c900b39c6ace51100d8aa6
Riccardo Caroli <beaver82@gmail.com>
2019-09-16 12:28:49 +0200

    chore: polyfills

--- dist/xtend.js
+++ dist/xtend.js
@@ -3,6 +3,10 @@
 @license MIT (https://github.com/minimit/xtend-library/blob/master/LICENSE) */
 
 // support explorer 15, firefox 48, opera 34, safari 10, chrome 47
+//<!-- // https://browser-update.org/ -->
+//<style>#buorg {visibility: visible}</style>
+//<script>var $buoop = {required: {e: 15, f: 48, o: 34, s: 10, c: 47}, reminder: 0, no_permanent_hide: true, api: 2019.05};</script>
+//<script src="//browser-update.org/update.min.js" defer></script>
 
 // support old browsers ie11+
 // ADD
