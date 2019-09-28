c4a78f0164ea895d9c1e5ed29c73cffc24354f4f
Riccardo Caroli <beaver82@gmail.com>
2019-09-28 06:25:15 +0200

    chore: removed btn--flex

--- src/gatsby/demos/inline/extensions/link/caret.js
+++ src/gatsby/demos/inline/extensions/link/caret.js
@@ -8,19 +8,21 @@ export const demo = {
 }
 
 demo.htmlSource = `
-<button type="button" class="btn caret" data-xt-toggle>
-  caret
-</button>
-
-<button type="button" class="btn caret caret--inverse" data-xt-toggle>
-  caret inverse
-</button>
-
-<button type="button" class="btn caret caret--horizontal" data-xt-toggle>
-  horizontal
-</button>
-
-<button type="button" class="btn caret caret--horizontal caret--inverse" data-xt-toggle>
-  horizontal inverse
-</button>
+<div class="list list-space--small align-items--center demo-source-from">
+  <button type="button" class="btn caret" data-xt-toggle>
+    caret
+  </button>
+  
+  <button type="button" class="btn caret caret--inverse" data-xt-toggle>
+    caret inverse
+  </button>
+  
+  <button type="button" class="btn caret caret--horizontal" data-xt-toggle>
+    horizontal
+  </button>
+  
+  <button type="button" class="btn caret caret--horizontal caret--inverse" data-xt-toggle>
+    horizontal inverse
+  </button>
+</div>
 `
