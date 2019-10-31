ddc2930cd278dbf1cd26c0561edb8e7269043176
Riccardo Caroli <beaver82@gmail.com>
2019-10-31 18:20:31 +0100

    docs: revision some styles

--- src/gatsby/code/inline/demos/loader/js-filler.js
+++ src/gatsby/code/inline/demos/loader/js-filler.js
@@ -16,10 +16,10 @@ demo.htmlSource = `
         <div class="card-block card-item">
           <div class="card-title">Lorem ipsum dolor sit amet</div>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie diam nec euismod commodo. Nunc ut fringilla nibh. Duis quis arcu quis neque tempor lobortis nec nec mauris. Proin vel elit pretium metus egestas congue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque hendrerit sagittis quam eget elementum. Vestibulum eu nulla nisl. Duis nec commodo tortor. Aenean feugiat, libero eget ultricies viverra, justo nunc efficitur lorem, at aliquet ante eros in est.</p>
-          <div class="loader loader--x loader-size--x loader--background loader--js demo--loader--js-filler">
-            <div class="filler">
-              <span></span><span></span>
-            </div>
+        </div>
+        <div class="loader loader--x loader-size--x loader--background loader--js demo--loader--js-filler">
+          <div class="filler">
+            <span></span><span></span>
           </div>
         </div>
       </div>
@@ -27,14 +27,12 @@ demo.htmlSource = `
   </div>
 
   <button type="button" class="btn btn--default">
-    <span>
-      Lorem ipsum
-    </span>
+    Lorem ipsum
     <span class="loader loader--x loader-size--x loader--background loader--js demo--loader--js-filler">
-        <span class="filler">
-          <span></span><span></span>
-        </span>
+      <span class="filler">
+        <span></span><span></span>
       </span>
+    </span>
   </button>
 </div>
 `
