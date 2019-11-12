fd4dee3f06c88a0de602499f6ea125264d81fe8c
Riccardo Caroli <beaver82@gmail.com>
2019-11-12 20:18:39 +0100

    docs: revision sidebar docs

--- src/gatsby/components/doc-sidebar.js
+++ src/gatsby/components/doc-sidebar.js
@@ -13,83 +13,79 @@ class DocSidebar extends React.Component {
         {page ? (
           <nav className="gatsby_site_article_sidebar_listing">
             <div className="container full">
-              <div className="gatsby_site_article_sidebar_inner">
-                {page.categories.category.map((category, i) => (
-                  <div key={i}>
-                    <div className="gatsby_site_article_sidebar_cat">
-                      <div
-                        className={`btn gatsby_btn--site_article_sidebar gatsby_btn--site_article_sidebar--cat ${
-                          !page.post.frontmatter.categories
-                            ? page.post.frontmatter.title === category.title.split('-').pop()
-                              ? 'active'
-                              : ''
-                            : page.post.frontmatter.categories.includes(category.title)
-                            ? 'current'
-                            : ''
-                        }`}
-                      >
-                        {category.title.split('-').pop()}
-                      </div>
-                      <div
-                        className={`gatsby_site_article_sidebar_sub active ${
-                          !page.post.frontmatter.categories
-                            ? page.post.frontmatter.title === category.title.split('-').pop()
-                              ? 'active'
-                              : ''
-                            : page.post.frontmatter.categories.includes(category.title)
+              {page.categories.category.map((category, i) => (
+                <div key={i}>
+                  <div className="gatsby_site_article_sidebar_cat">
+                    <div
+                      className={`gatsby_cat--site_article_sidebar ${
+                        !page.post.frontmatter.categories
+                          ? page.post.frontmatter.title === category.title.split('-').pop()
                             ? 'active'
                             : ''
-                        }`}
-                      >
-                        <div className="gatsby_site_article_sidebar_item">
-                          <div className="gatsby_site_article_sidebar_line"></div>
-                          {category.posts.map(({ post }, z) =>
-                            post.frontmatter.parent === post.frontmatter.title ? (
-                              <div key={z}>
-                                <Link
-                                  to={markdownSlug(post)}
-                                  className={`btn gatsby_btn--site_article_sidebar gatsby_btn--site_article_sidebar--sub ${
-                                    markdownSlug(page.post) === markdownSlug(post)
-                                      ? 'active'
-                                      : page.post.frontmatter.parent === post.frontmatter.parent
-                                      ? 'current'
-                                      : ''
-                                  }`}
-                                >
-                                  {post.frontmatter.title}
-                                </Link>
-                                {post.frontmatter.parent === page.post.frontmatter.parent ? (
-                                  <div className="gatsby_site_article_sidebar_adiacent active">
-                                    <div className="gatsby_site_article_sidebar_item">
-                                      <div className="gatsby_site_article_sidebar_line"></div>
-                                      {page.postsAdiacent.posts.map(({ post: adiacent }, i) =>
-                                        adiacent.frontmatter.title !== post.frontmatter.parent ? (
-                                          !adiacent.frontmatter.demos ? (
-                                            <div key={i}>
-                                              <Link
-                                                to={markdownSlug(adiacent)}
-                                                className={`btn gatsby_btn--site_article_sidebar gatsby_btn--site_article_sidebar--adiacent ${
-                                                  page.post.frontmatter.title === adiacent.frontmatter.title ? 'active' : ''
-                                                }`}
-                                              >
-                                                {adiacent.frontmatter.title}
-                                              </Link>
-                                            </div>
-                                          ) : null
+                          : page.post.frontmatter.categories.includes(category.title)
+                          ? 'current'
+                          : ''
+                      }`}
+                    >
+                      {category.title.split('-').pop()}
+                    </div>
+                    <div
+                      className={`gatsby_site_article_sidebar_sub active ${
+                        !page.post.frontmatter.categories
+                          ? page.post.frontmatter.title === category.title.split('-').pop()
+                            ? 'active'
+                            : ''
+                          : page.post.frontmatter.categories.includes(category.title)
+                          ? 'active'
+                          : ''
+                      }`}
+                    >
+                      <div className="gatsby_site_article_sidebar_item">
+                        {category.posts.map(({ post }, z) =>
+                          post.frontmatter.parent === post.frontmatter.title ? (
+                            <div key={z}>
+                              <Link
+                                to={markdownSlug(post)}
+                                className={`btn gatsby_btn--site_article_sidebar gatsby_btn--site_article_sidebar--sub ${
+                                  markdownSlug(page.post) === markdownSlug(post)
+                                    ? 'active'
+                                    : page.post.frontmatter.parent === post.frontmatter.parent
+                                    ? 'current'
+                                    : ''
+                                }`}
+                              >
+                                {post.frontmatter.title}
+                              </Link>
+                              {post.frontmatter.parent === page.post.frontmatter.parent ? (
+                                <div className="gatsby_site_article_sidebar_adiacent active">
+                                  <div className="gatsby_site_article_sidebar_item">
+                                    {page.postsAdiacent.posts.map(({ post: adiacent }, i) =>
+                                      adiacent.frontmatter.title !== post.frontmatter.parent ? (
+                                        !adiacent.frontmatter.demos ? (
+                                          <div key={i}>
+                                            <Link
+                                              to={markdownSlug(adiacent)}
+                                              className={`btn gatsby_btn--site_article_sidebar gatsby_btn--site_article_sidebar--adiacent ${
+                                                page.post.frontmatter.title === adiacent.frontmatter.title ? 'active' : ''
+                                              }`}
+                                            >
+                                              {adiacent.frontmatter.title}
+                                            </Link>
+                                          </div>
                                         ) : null
-                                      )}
-                                    </div>
+                                      ) : null
+                                    )}
                                   </div>
-                                ) : null}
-                              </div>
-                            ) : null
-                          )}
-                        </div>
+                                </div>
+                              ) : null}
+                            </div>
+                          ) : null
+                        )}
                       </div>
                     </div>
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
           </nav>
         ) : null}
