42d0376cf248215ea64d63ff163a800a81b97bd3
Riccardo Caroli <beaver82@gmail.com>
2019-12-30 17:21:39 +0100

    chore: copy

--- src/gatsby/components/pages/index.js
+++ src/gatsby/components/pages/index.js
@@ -13,6 +13,8 @@ class Page extends React.Component {
         <SEO title={seo.title + ' — ' + seo.description} />
         <ul>
           <li>UI framework</li>
+          <li>vanilla html components with js and less imports</li>
+          <li>extremely customizable</li>
           <li>simple solutions for hard problems</li>
           <li>modular css and javascript for html apps</li>
           <li>(vanilla > simple > custom) library with extensible (markup > styles > javascript)</li>
@@ -20,11 +22,12 @@ class Page extends React.Component {
           <li>advanced vanilla js with customizzable behaviour and animations</li>
           <li>a new approach to custom frontend development</li>
           <li>ecosystem of vanilla js plugins</li>
-          <li>focus on interactions and animations</li>
+          <li>focused on interactions and animations</li>
           <li>focused on working speed, quality and customization</li>
           <li>highly customizzable library for who wants to customize the css without many variables or mixins</li>
           <li>includes core styles not to be modified and a separate smart theme template for custom styles, easy to use, extensible, intelligible code, contextualized code</li>
           <li>automatic aria attributes</li>
+          <li>style real css, not a list of variables</li>
         </ul>
       </Layout>
     )
