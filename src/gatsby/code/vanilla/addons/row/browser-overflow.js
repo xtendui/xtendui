506d0e496f455493c8f6dca10060ede7de347533
Riccardo Caroli <beaver82@gmail.com>
2020-02-12 17:12:09 +0100

    docs: row vrowser overflow

--- src/gatsby/code/vanilla/addons/row/browser-overflow.js
+++ src/gatsby/code/vanilla/addons/row/browser-overflow.js
@@ -9,39 +9,8 @@ export const demo = {
 }
 
 demo.htmlSource = `
-<div class="row row-default row-browser-overflow">
-
-  <div class="col-12 col-4-sm">
-
-    <div class="card card-default">
-      <div class="card-design"></div>
-      <div class="card-inner">
-        <div class="card-content">
-          <div class="card-block card-item">
-            <div class="card-title">Lorem ipsum dolor sit amet</div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie diam nec euismod commodo. Nunc ut fringilla nibh. Duis quis arcu quis neque tempor lobortis nec nec mauris. Proin vel elit pretium metus egestas congue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque hendrerit sagittis quam eget elementum. Vestibulum eu nulla nisl. Duis nec commodo tortor. Aenean feugiat, libero eget ultricies viverra, justo nunc efficitur lorem, at aliquet ante eros in est.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </div>
-
-  <div class="col-12 col-8-sm">
-
-    <div class="card card-default">
-      <div class="card-design"></div>
-      <div class="card-inner">
-        <div class="card-content">
-          <div class="card-block card-item">
-            <div class="card-title">Lorem ipsum dolor sit amet</div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie diam nec euismod commodo. Nunc ut fringilla nibh. Duis quis arcu quis neque tempor lobortis nec nec mauris. Proin vel elit pretium metus egestas congue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque hendrerit sagittis quam eget elementum. Vestibulum eu nulla nisl. Duis nec commodo tortor. Aenean feugiat, libero eget ultricies viverra, justo nunc efficitur lorem, at aliquet ante eros in est.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </div>
+<div class="row row-default row-browser-overflow"
+    style="align-items: stretch;">
 
   <div class="col-12 col-8-sm">
 
@@ -60,6 +29,38 @@ demo.htmlSource = `
   </div>
 
   <div class="col-12 col-4-sm">
+
+    <div class="card card-default">
+      <div class="card-design"></div>
+      <div class="card-inner">
+        <div class="card-content">
+          <div class="card-block card-item">
+            <div class="card-title">Lorem ipsum dolor sit amet</div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="col-12 col-4-sm">
+
+    <div class="card card-default">
+      <div class="card-design"></div>
+      <div class="card-inner">
+        <div class="card-content">
+          <div class="card-block card-item">
+            <div class="card-title">Lorem ipsum dolor sit amet</div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="col-12 col-8-sm">
 
     <div class="card card-default">
       <div class="card-design"></div>
