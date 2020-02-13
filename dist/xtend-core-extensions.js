49d3139e28727a9b677745c37c93006a2e64bfda
Riccardo Caroli <beaver82@gmail.com>
2020-02-13 10:02:11 +0100

    Revert "fix(js): removed ie11 support"
    This reverts commit fd756ccef74a4e77d9fb7c51066f8e5599017796.

--- dist/xtend-core-extensions.js
+++ dist/xtend-core-extensions.js
@@ -5,6 +5,7 @@
 import 'core-js'
 import 'regenerator-runtime/runtime'
 import 'xtend-library/src/polyfill.js'
+import 'xtend-library/src/polyfill-old.js'
 import { Xt } from 'xtend-library'
 import 'xtend-library/src/xtend-core.js'
 import 'xtend-library/src/xtend-extensions.js'
