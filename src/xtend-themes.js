c0fece55cb3752c0698cbdec6e7d5ee82c03528f
Riccardo Caroli <rcaroli@webgriffe.com>
2020-02-27 16:01:34 +0100

    docs: theme listing bertani 2020

--- src/xtend-themes.js
+++ src/xtend-themes.js
@@ -1,5 +1,6 @@
 if (typeof window !== 'undefined') {
   require('xtend-library/src/themes/drop/2020-euroricambi.js')
+  require('xtend-library/src/themes/listing/2020-bertani.js')
   require('xtend-library/src/themes/overlay/2020-euroricambi.js')
   require('xtend-library/src/themes/slider/2019-sun68.js')
   require('xtend-library/src/themes/slider/2019-webgriffe.js')
