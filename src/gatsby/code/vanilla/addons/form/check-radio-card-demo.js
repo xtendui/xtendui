57f1e4f414584065978ce98acab3fcced38fd126
Riccardo Caroli <rcaroli@webgriffe.com>
2020-04-03 17:14:26 +0200

    docs: form addon check radio card

--- src/gatsby/code/vanilla/addons/form/check-radio-card-demo.js
+++ src/gatsby/code/vanilla/addons/form/check-radio-card-demo.js
@@ -14,19 +14,17 @@ demo.htmlSource = `
 
     <div class="col-12 col-6-sm">
       <input type="checkbox" id="check-card-0">
-      <label for="check-card-0" class="align-center">
-        <div class="card card-default">
-          <div class="card-design"></div>
-          <div class="card-inner">
-            <div class="card-content">
-              <div class="card-asset">
-                <div class="media-container" style="padding-bottom:37.5%"><div class="media-inner"><img class="media media-cover" src="/img.svg" alt loading="lazy"/></div></div>
-              </div>
-              <div class="card-block card-item">
-                <div class="card-title">Lorem ipsum</div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non feugiat lorem, nec volutpat turpis. Sed pulvinar hendrerit mauris at pharetra. Suspendisse vel aliquam quam, non tincidunt sem.</p>
-                <div class="form-label"></div>
-              </div>
+      <label for="check-card-0" class="card card-default align-center">
+        <div class="card-design"></div>
+        <div class="card-inner">
+          <div class="card-content">
+            <div class="card-asset">
+              <div class="media-container" style="padding-bottom:37.5%"><div class="media-inner"><img class="media media-cover" src="/img.svg" alt loading="lazy"/></div></div>
+            </div>
+            <div class="card-block card-item">
+              <div class="card-title">Lorem ipsum</div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non feugiat lorem, nec volutpat turpis. Sed pulvinar hendrerit mauris at pharetra. Suspendisse vel aliquam quam, non tincidunt sem.</p>
+              <div class="form-label"></div>
             </div>
           </div>
         </div>
@@ -35,19 +33,17 @@ demo.htmlSource = `
 
     <div class="col-12 col-6-sm">
       <input type="checkbox" id="check-card-1">
-      <label for="check-card-1" class="align-center">
-        <div class="card card-default">
-          <div class="card-design"></div>
-          <div class="card-inner">
-            <div class="card-content">
-              <div class="card-asset">
-                <div class="media-container" style="padding-bottom:37.5%"><div class="media-inner"><img class="media media-cover" src="/img.svg" alt loading="lazy"/></div></div>
-              </div>
-              <div class="card-block card-item">
-                <div class="card-title">Lorem ipsum</div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non feugiat lorem, nec volutpat turpis. Sed pulvinar hendrerit mauris at pharetra. Suspendisse vel aliquam quam, non tincidunt sem.</p>
-                <div class="form-label"></div>
-              </div>
+      <label for="check-card-1" class="card card-default align-center">
+        <div class="card-design"></div>
+        <div class="card-inner">
+          <div class="card-content">
+            <div class="card-asset">
+              <div class="media-container" style="padding-bottom:37.5%"><div class="media-inner"><img class="media media-cover" src="/img.svg" alt loading="lazy"/></div></div>
+            </div>
+            <div class="card-block card-item">
+              <div class="card-title">Lorem ipsum</div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non feugiat lorem, nec volutpat turpis. Sed pulvinar hendrerit mauris at pharetra. Suspendisse vel aliquam quam, non tincidunt sem.</p>
+              <div class="form-label"></div>
             </div>
           </div>
         </div>
@@ -56,19 +52,17 @@ demo.htmlSource = `
 
     <div class="col-12 col-6-sm">
       <input type="radio" id="radio-card-0" name="radio-card">
-      <label for="radio-card-0" class="align-center">
-        <div class="card card-default">
-          <div class="card-design"></div>
-          <div class="card-inner">
-            <div class="card-content">
-              <div class="card-asset">
-                <div class="media-container" style="padding-bottom:37.5%"><div class="media-inner"><img class="media media-cover" src="/img.svg" alt loading="lazy"/></div></div>
-              </div>
-              <div class="card-block card-item">
-                <div class="card-title">Lorem ipsum</div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non feugiat lorem, nec volutpat turpis. Sed pulvinar hendrerit mauris at pharetra. Suspendisse vel aliquam quam, non tincidunt sem.</p>
-                <div class="form-label"></div>
-              </div>
+      <label for="radio-card-0" class="card card-default align-center">
+        <div class="card-design"></div>
+        <div class="card-inner">
+          <div class="card-content">
+            <div class="card-asset">
+              <div class="media-container" style="padding-bottom:37.5%"><div class="media-inner"><img class="media media-cover" src="/img.svg" alt loading="lazy"/></div></div>
+            </div>
+            <div class="card-block card-item">
+              <div class="card-title">Lorem ipsum</div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non feugiat lorem, nec volutpat turpis. Sed pulvinar hendrerit mauris at pharetra. Suspendisse vel aliquam quam, non tincidunt sem.</p>
+              <div class="form-label"></div>
             </div>
           </div>
         </div>
@@ -77,19 +71,17 @@ demo.htmlSource = `
 
     <div class="col-12 col-6-sm">
       <input type="radio" id="radio-card-1" name="radio-card">
-      <label for="radio-card-1" class="align-center">
-        <div class="card card-default">
-          <div class="card-design"></div>
-          <div class="card-inner">
-            <div class="card-content">
-              <div class="card-asset">
-                <div class="media-container" style="padding-bottom:37.5%"><div class="media-inner"><img class="media media-cover" src="/img.svg" alt loading="lazy"/></div></div>
-              </div>
-              <div class="card-block card-item">
-                <div class="card-title">Lorem ipsum</div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non feugiat lorem, nec volutpat turpis. Sed pulvinar hendrerit mauris at pharetra. Suspendisse vel aliquam quam, non tincidunt sem.</p>
-                <div class="form-label"></div>
-              </div>
+      <label for="radio-card-1" class="card card-default align-center">
+        <div class="card-design"></div>
+        <div class="card-inner">
+          <div class="card-content">
+            <div class="card-asset">
+              <div class="media-container" style="padding-bottom:37.5%"><div class="media-inner"><img class="media media-cover" src="/img.svg" alt loading="lazy"/></div></div>
+            </div>
+            <div class="card-block card-item">
+              <div class="card-title">Lorem ipsum</div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non feugiat lorem, nec volutpat turpis. Sed pulvinar hendrerit mauris at pharetra. Suspendisse vel aliquam quam, non tincidunt sem.</p>
+              <div class="form-label"></div>
             </div>
           </div>
         </div>
