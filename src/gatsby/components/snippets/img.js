08285a54134d8c41ca76e3d5df06f6cc2cb2bf0c
Riccardo Caroli <rcaroli@webgriffe.com>
2020-05-15 16:13:51 +0200

    docs: fix demos zindex + fix img attributes empty

--- src/gatsby/components/snippets/img.js
+++ src/gatsby/components/snippets/img.js
@@ -3,5 +3,5 @@
 
 export default ({ classes = '', ratio = null, height = null }) => {
   // prettier-ignore
-  return `<div class="media-container" style="${ratio ? 'padding-bottom:' + ratio : ''}${height ? 'height:' + height : ''}"><div class="media-inner"><img class="media ${classes}" src="/img.svg" alt="" loading="lazy"/></div></div>`
+  return `<div class="media-container"${ ratio ? ' style="padding-bottom:' + ratio + '"' : height ? ' style="height:' + height + '"' : '' }><div class="media-inner"><img class="media ${classes}" src="/img.svg" alt="" loading="lazy"/></div></div>`
 }
