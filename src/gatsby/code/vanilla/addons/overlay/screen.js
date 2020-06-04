40be6582d3bb19d22902f67fcb8172ef45f2e4e9
Riccardo Caroli <rcaroli@webgriffe.com>
2020-06-04 15:55:21 +0200

    docs: demos overlay screen

--- src/gatsby/code/vanilla/addons/overlay/screen.js
+++ src/gatsby/code/vanilla/addons/overlay/screen.js
@@ -95,5 +95,122 @@ demo.htmlSource = `
     </div>
   </div>
 
+  <div data-xt-overlay>
+    <button type="button" class="btn btn-primary">
+      screen image full
+    </button>
+    <div class="overlay overlay-screen overlay-full">
+      <div class="overlay-container">
+        <div class="overlay-inner">
+
+          <div class="card card-overlay">
+            <div class="card-design"></div>
+            <div class="btn btn-close" aria-label="Close"></div>
+            <div class="card-inner">
+              <div class="card-content">
+                <div class="card-asset">
+                  ${img({ classes: 'media-cover', ratio: '100%' })}
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div data-xt-overlay>
+    <button type="button" class="btn btn-primary">
+      screen group side image
+    </button>
+    <div class="overlay overlay-screen">
+      <div class="overlay-container">
+        <div class="overlay-inner">
+
+          <div class="card card-overlay">
+            <div class="card-design"></div>
+            <div class="btn btn-close" aria-label="Close"></div>
+            <div class="card-inner">
+              <div class="card-content">
+                <div class="card-item-nested card-item card-group-sm">
+                  <div class="card-block card-item">
+                    <div class="card-title">Lorem ipsum</div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
+                      ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.</p>
+                    <p>Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                      justo.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
+                      ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.</p>
+                    <p>Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                      justo.</p>
+                  </div>
+                  <div class="card-item card-block-side col-6-sm">
+                    <div class="card-asset">
+                      ${img({ classes: 'media-cover', ratio: '37.5%' })}
+                    </div>
+                    <div class="card-block card-item">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
+                        ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.</p>
+                      <p>Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                        justo.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div data-xt-overlay>
+    <button type="button" class="btn btn-primary">
+      screen full group side image
+    </button>
+    <div class="overlay overlay-screen overlay-full">
+      <div class="overlay-container">
+        <div class="overlay-inner">
+
+          <div class="card card-overlay">
+            <div class="card-design"></div>
+            <div class="btn btn-close" aria-label="Close"></div>
+            <div class="card-inner">
+              <div class="card-content">
+                <div class="card-item-nested card-item card-group-sm">
+                  <div class="card-block card-item">
+                    <div class="card-title">Lorem ipsum</div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
+                      ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.</p>
+                    <p>Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                      justo.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
+                      ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.</p>
+                    <p>Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                      justo.</p>
+                  </div>
+                  <div class="card-item card-block-side col-6-sm">
+                    <div class="card-asset">
+                      ${img({ classes: 'media-cover', ratio: '37.5%' })}
+                    </div>
+                    <div class="card-block card-item">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
+                        ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.</p>
+                      <p>Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                        justo.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
 </div>
 `
