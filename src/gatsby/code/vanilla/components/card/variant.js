acfbb810d619c0263d1bfad9057ad5ea1bbdc21f
Riccardo Caroli <rcaroli@webgriffe.com>
2020-06-17 10:40:44 +0200

    fix(overlay): removed card design animations overlay and added overlay design

--- src/gatsby/code/vanilla/components/card/variant.js
+++ src/gatsby/code/vanilla/components/card/variant.js
@@ -17,7 +17,7 @@ demo.htmlSource = `
       <div class="card-inner">
         <div class="card-content">
           <div class="card-block card-item">
-            <div class="card-title">Primary</div>
+            <div class="card-title">Default</div>
             <p><strong>Lorem ipsum</strong> dolor sit amet, consectetur <a href="#">adipiscing elit</a>. Nullam suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris augue, sagittis vitae magna eget, vehicula scelerisque elit.</p>
           </div>
         </div>
@@ -31,7 +31,7 @@ demo.htmlSource = `
       <div class="card-inner">
         <div class="card-content">
           <div class="card-block card-item">
-            <div class="card-title">Default</div>
+            <div class="card-title">Primary</div>
             <p><strong>Lorem ipsum</strong> dolor sit amet, consectetur <a href="#">adipiscing elit</a>. Nullam suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris augue, sagittis vitae magna eget, vehicula scelerisque elit.</p>
           </div>
         </div>
