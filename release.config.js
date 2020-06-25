8d44249a49e20db09976d0fcd28479ab0976f582
Riccardo Caroli <rcaroli@webgriffe.com>
2020-06-25 12:43:04 +0200

    chore: release

--- release.config.js
+++ release.config.js
@@ -15,7 +15,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md', '/**'],
         message: 'chore(release): ${nextRelease.version} [skip ci]',
       },
     ],
