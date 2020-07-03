e851c6eb8efc3d388dc94e6a46e96c4896bea79b
Riccardo Caroli <rcaroli@webgriffe.com>
2020-07-03 07:07:54 +0200

    chore: docs

--- src/gatsby/assets/scripts/gatsby.js
+++ src/gatsby/assets/scripts/gatsby.js
@@ -1,13 +1,12 @@
 const gatsbySidebar = () => {
   requestAnimationFrame(() => {
     const sidebarArticle = document.querySelector('.gatsby_site_article_sidebar')
-    let sidebarActive = document.querySelector('.gatsby_btn-site_article_sidebar--sub.active')
+    let sidebarActive = document.querySelector('.gatsby_btn-site_article_sidebar--sub.active, .gatsby_btn-site_article_sidebar--sub.current')
     if (sidebarActive) {
       const add = 25
       sidebarActive = sidebarActive.closest('.gatsby_site_header_item_container')
       const sidebarActiveTop = sidebarActive.getBoundingClientRect().top + sidebarArticle.scrollTop
       const sidebarActiveBottom = sidebarActiveTop + sidebarActive.offsetHeight
-      console.log(sidebarArticle.scrollTop, sidebarActiveTop, sidebarArticle.scrollTop + sidebarArticle.offsetHeight, sidebarActiveBottom)
       if (sidebarArticle.scrollTop > sidebarActiveTop) {
         sidebarArticle.scrollTop = sidebarActiveTop - add
       }
