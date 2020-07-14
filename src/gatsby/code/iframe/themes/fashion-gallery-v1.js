75cc191487118915c43e96e4912f0ef03a58b207
minimit <beaver82@gmail.com>
2020-07-14 21:09:02 +0200

    docs: theme fashion gallery revision

--- src/gatsby/code/iframe/themes/fashion-gallery-v1.js
+++ src/gatsby/code/iframe/themes/fashion-gallery-v1.js
@@ -13,161 +13,189 @@ export const demo = {
 }
 
 demo.htmlSource = `
-<div class="product">
+<div class="site_wrapper">
 
-  <div class="container">
-    <div class="product_inner">
-      <div class="row">
-      
-        <div class="product_left_col">
-          <div class="product_left_outer">
-            <div class="product_left">
-            
-              <div class="product_images">
-                <div class="list-block">
-                  <a href="#product-image-overlay-0" class="product_image">
-                    <div class="product_image_inner" data-xt-overlay="{ targets: '#overlay--product_images' }">
-                      <img id="product-image-0" src="/img.svg" loading="lazy" alt=""/>
+  <header class="site_header">
+    <div class="card card-default card-squared">
+      <div class="card-design"></div>
+      <div class="card-inner">
+        <div class="card-content">
+          <div class="card-block card-item" style="padding: 4rem 0; text-align: center;">
+            <div class="card-title">Header</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main class="site_main">
+    <div class="product">
+
+      <div class="container">
+        <div class="product_inner">
+          <div class="row">
+
+            <div class="product_left_col">
+              <div class="product_left_outer">
+                <div class="product_left">
+
+                  <div class="product_images">
+                    <div class="list-block">
+                      <a href="#product-image-overlay-0" class="product_image">
+                        <div class="product_image_inner" data-xt-overlay="{ targets: '#overlay--product_images' }">
+                          <img id="product-image-0" src="/img.svg" loading="lazy" alt=""/>
+                        </div>
+                      </a>
+                      <a href="#product-image-overlay-1" class="product_image">
+                        <div class="product_image_inner" data-xt-overlay="{ targets: '#overlay--product_images' }">
+                          <img id="product-image-1" src="/img.svg" loading="lazy" alt=""/>
+                        </div>
+                      </a>
+                      <a href="#product-image-overlay-3" class="product_image">
+                        <div class="product_image_inner" data-xt-overlay="{ targets: '#overlay--product_images' }">
+                          <img id="product-image-2" src="/img.svg" loading="lazy" alt=""/>
+                        </div>
+                      </a>
+                      <a href="#product-image-overlay-4" class="product_image">
+                        <div class="product_image_inner" data-xt-overlay="{ targets: '#overlay--product_images' }">
+                          <img id="product-image-3" src="/img.svg" loading="lazy" alt=""/>
+                        </div>
+                      </a>
+                      <a href="#product-image-overlay-4" class="product_image">
+                        <div class="product_image_inner" data-xt-overlay="{ targets: '#overlay--product_images' }">
+                          <img id="product-image-4" src="/img.svg" loading="lazy" alt=""/>
+                        </div>
+                      </a>
                     </div>
-                  </a>
-                  <a href="#product-image-overlay-1" class="product_image">
-                    <div class="product_image_inner" data-xt-overlay="{ targets: '#overlay--product_images' }">
-                      <img id="product-image-1" src="/img.svg" loading="lazy" alt=""/>
+                  </div>
+
+                  <div class="product_images_nav">
+                    <div class="list-block">
+                      <a href="#product-image-0" class="product_images_nav_item" title="Go to image 0">
+                        <div class="product_images_nav_item_inner">
+                          <span></span>
+                        </div>
+                      </a>
+                      <a href="#product-image-1" class="product_images_nav_item" title="Go to image 1">
+                        <div class="product_images_nav_item_inner">
+                          <span></span>
+                        </div>
+                      </a>
+                      <a href="#product-image-2" class="product_images_nav_item" title="Go to image 2">
+                        <div class="product_images_nav_item_inner">
+                          <span></span>
+                        </div>
+                      </a>
+                      <a href="#product-image-3" class="product_images_nav_item" title="Go to image 3">
+                        <div class="product_images_nav_item_inner">
+                          <span></span>
+                        </div>
+                      </a>
+                      <a href="#product-image-4" class="product_images_nav_item" title="Go to image 3">
+                        <div class="product_images_nav_item_inner">
+                          <span></span>
+                        </div>
+                      </a>
                     </div>
-                  </a>
-                  <a href="#product-image-overlay-3" class="product_image">
-                    <div class="product_image_inner" data-xt-overlay="{ targets: '#overlay--product_images' }">
-                      <img id="product-image-2" src="/img.svg" loading="lazy" alt=""/>
-                    </div>
-                  </a>
-                  <a href="#product-image-overlay-4" class="product_image">
-                    <div class="product_image_inner" data-xt-overlay="{ targets: '#overlay--product_images' }">
-                      <img id="product-image-3" src="/img.svg" loading="lazy" alt=""/>
-                    </div>
-                  </a>
-                  <a href="#product-image-overlay-4" class="product_image">
-                    <div class="product_image_inner" data-xt-overlay="{ targets: '#overlay--product_images' }">
-                      <img id="product-image-4" src="/img.svg" loading="lazy" alt=""/>
-                    </div>
-                  </a>
-                </div>
-              </div>
-              
-              <div class="product_images_nav">
-                <div class="list-block">
-                  <a href="#product-image-0" class="product_images_nav_item" title="Go to image 0">
-                    <div class="product_images_nav_item_inner">
-                      <span></span>
-                    </div>
-                  </a>
-                  <a href="#product-image-1" class="product_images_nav_item" title="Go to image 1">
-                    <div class="product_images_nav_item_inner">
-                      <span></span>
-                    </div>
-                  </a>
-                  <a href="#product-image-2" class="product_images_nav_item" title="Go to image 2">
-                    <div class="product_images_nav_item_inner">
-                      <span></span>
-                    </div>
-                  </a>
-                  <a href="#product-image-3" class="product_images_nav_item" title="Go to image 3">
-                    <div class="product_images_nav_item_inner">
-                      <span></span>
-                    </div>
-                  </a>
-                  <a href="#product-image-4" class="product_images_nav_item" title="Go to image 3">
-                    <div class="product_images_nav_item_inner">
-                      <span></span>
-                    </div>
-                  </a>
-                </div>
-              </div>
-              
-              <div class="overlay overlay-default overlay-screen overlay-size-full" id="overlay--product_images">
-                <div class="overlay-container">
-                  <div class="overlay-inner">
-                    <div class="overlay-design"></div>
-                    <div class="card card-overlay">
-                      <div class="card-design"></div>
-                      <div class="btn btn-close" aria-label="Close"></div>
-                      <div class="card-inner">
-                        <div class="card-content">
-                          <div class="card-asset">
-                            <div class="list-block">
-                              <img id="product-image-overlay-0" src="/img.svg" loading="eager" alt=""/>
-                              <img id="product-image-overlay-1" src="/img.svg" loading="eager" alt=""/>
-                              <img id="product-image-overlay-2" src="/img.svg" loading="eager" alt=""/>
-                              <img id="product-image-overlay-3" src="/img.svg" loading="eager" alt=""/>
-                              <img id="product-image-overlay-4" src="/img.svg" loading="eager" alt=""/>
+                  </div>
+
+                  <div class="overlay overlay-default overlay-screen overlay-size-full" id="overlay--product_images">
+                    <div class="overlay-container">
+                      <div class="overlay-inner">
+                        <div class="overlay-design"></div>
+                        <div class="card card-overlay">
+                          <div class="card-design"></div>
+                          <div class="btn btn-close" aria-label="Close"></div>
+                          <div class="card-inner">
+                            <div class="card-content">
+                              <div class="card-asset">
+                                <div class="list-block">
+                                  <img id="product-image-overlay-0" src="/img.svg" loading="eager" alt=""/>
+                                  <img id="product-image-overlay-1" src="/img.svg" loading="eager" alt=""/>
+                                  <img id="product-image-overlay-2" src="/img.svg" loading="eager" alt=""/>
+                                  <img id="product-image-overlay-3" src="/img.svg" loading="eager" alt=""/>
+                                  <img id="product-image-overlay-4" src="/img.svg" loading="eager" alt=""/>
+                                </div>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
+
                 </div>
               </div>
-            
             </div>
+
+            <div class="product_right_col" id="header-continue-target">
+              <div class="product_right">
+
+                <h1 class="product_title">
+                  Lorem ipsum dolor sit amet
+                </h1>
+
+                <div class="product_price">
+                  €&nbsp;229,00
+                </div>
+
+                <div class="product_actions">
+                  <a href="#" class="btn btn-primary btn-big btn-wide btn-tall">
+                  Add to cart
+                  </a>
+                </div>
+
+                <div class="product_details">
+                  <p><strong>Lorem ipsum</strong> dolor sit amet, consectetur adipiscing elit. Nullam suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris augue, sagittis vitae magna eget, vehicula scelerisque elit.</p>
+                </div>
+
+                <div class="product_attributes">
+                  <dl class="dl-line">
+                    <dt>
+                      Product Code:
+                    </dt>
+                    <dd>
+                      012345
+                    </dd>
+                    <dt>
+                      Internal Code:
+                    </dt>
+                    <dd>
+                      543210
+                    </dd>
+                  </dl>
+                </div>
+
+              </div>
+            </div>
+
           </div>
         </div>
-        
-        <div class="product_right_col" id="header-continue-target">
-          <div class="product_right">
-          
-            <h1 class="product_title">
-              Lorem ipsum dolor sit amet
-            </h1>
-            
-            <div class="product_price">
-              €&nbsp;229,00
-            </div>
-            
-            <div class="product_actions">
-              <a href="#" class="btn btn-primary btn-big btn-wide btn-tall">
-              Add to cart
-              </a>
-            </div>
-            
-            <div class="product_details">
-              <p><strong>Lorem ipsum</strong> dolor sit amet, consectetur adipiscing elit. Nullam suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris augue, sagittis vitae magna eget, vehicula scelerisque elit.</p>
-            </div>
-            
-            <div class="product_attributes">
-              <dl class="dl-line">
-                <dt>
-                  Product Code:
-                </dt>
-                <dd>
-                  012345
-                </dd>
-                <dt>
-                  Internal Code:
-                </dt>
-                <dd>
-                  543210
-                </dd>
-              </dl>
-            </div>
-            
+      </div>
+
+      <a href="#header-continue-target" class="header-continue">
+        <span class="icon-xt-chevron-down"></span>
+      </a>
+
+      <div class="product_limit-bottom"></div>
+
+    </div>
+  </main>
+
+  <footer class="site_footer">
+    <div class="card card-default card-squared">
+      <div class="card-design"></div>
+      <div class="card-inner">
+        <div class="card-content">
+          <div class="card-block card-item" style="padding: 30rem 0; text-align: center;">
+            <div class="card-title">Footer</div>
           </div>
         </div>
-        
       </div>
     </div>
-  </div>
-  
-  <a href="#header-continue-target" class="header-continue">
-    <span class="icon-xt-chevron-down"></span>
-  </a>
-  
-  <div class="product_limit-bottom"></div>
-  
-</div>
+  </footer>
 
-<br/><br/><br/><br/><br/><br/><br/><br/><br/>
-<br/><br/><br/><br/><br/><br/><br/><br/><br/>
-<br/><br/><br/><br/><br/><br/><br/><br/><br/>
+</div>
 `
 
 class Page extends React.Component {
