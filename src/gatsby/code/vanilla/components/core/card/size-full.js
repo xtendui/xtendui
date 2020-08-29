b02df0cbd5fbb87911f86ea4c6f09adbb92b1513
minimit <beaver82@gmail.com>
2020-08-29 16:44:50 +0200

    chore: tailwind card sizes

--- src/gatsby/code/vanilla/components/core/card/size-full.js
+++ src/gatsby/code/vanilla/components/core/card/size-full.js
@@ -38,5 +38,22 @@ demo.htmlSource = `
     </div>
   </div>
 
+  <div class="w-full sm:w-6/12">
+    <div class="card ${cardDefaultFull()}">
+      <div class="card-block card-block-small p-0">
+        <div class="h5">Small</div>
+        <p><strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris augue, sagittis vitae magna eget, vehicula scelerisque elit.</p>
+      </div>
+      <div class="card-block card-block-medium px-0 pb-0">
+        <div class="h4">Medium</div>
+        <p><strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris augue, sagittis vitae magna eget, vehicula scelerisque elit.</p>
+      </div>
+      <div class="card-block card-block-large px-0 pb-0">
+        <div class="h3">Large</div>
+        <p><strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris augue, sagittis vitae magna eget, vehicula scelerisque elit.</p>
+      </div>
+    </div>
+  </div>
+
 </div>
 `
