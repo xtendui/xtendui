60581d86724c17d4d56a3db97626f074f29e8e2b
Riccardo Caroli <rcaroli@webgriffe.com>
2020-10-02 06:25:16 +0200

    chore: addon multilevel

--- src/gatsby/code/vanilla/components/addons/toggle/multilevel.js
+++ src/gatsby/code/vanilla/components/addons/toggle/multilevel.js
@@ -8,145 +8,143 @@ export const demo = {
 }
 
 demo.htmlSource = `
-<div class="demo--multilevel">
-  <div class="multilevel">
+<div class="multilevel">
 
-    <nav role="navigation" class="multilevel-tab">
+  <nav role="navigation" class="toggle" data-multilevel-tab>
 
-      <div class="multilevel-title-wrapper">
-        <div class="h4 multilevel-title">
-          Products
-        </div>
-        <button type="button" class="btn btn-none multilevel-reset hidden multilevel-tab-reset">
-          Back
-        </button>
+    <div class="h5 flex items-center justify-between">
+      <div>
+        Products
       </div>
+      <button type="button" class="btn link hidden" data-multilevel-reset>
+        Back
+      </button>
+    </div>
 
-      <nav class="list-block multilevel-list">
-        <button type="button" class="btn" data-xt-group="cat-0">
-          Category 0
-        </button>
-        <button type="button" class="btn" data-xt-group="cat-1">
-          Category 1
-        </button>
-        <a href="#" class="btn">
-          Link 0
-        </a>
-        <a href="#" class="btn">
-          Link 1
-        </a>
-      </nav>
-
-      <div class="multilevel-title-wrapper">
-        <div class="h4 multilevel-title">
-          About
-        </div>
-      </div>
-      <nav class="list-block multilevel-list">
-        <a href="#" class="btn">
-          Link 0
-        </a>
-        <a href="#" class="btn">
-          Link 1
-        </a>
-      </nav>
-
+    <nav class="list-block multilevel-list">
+      <button type="button" class="btn link" data-xt-group="cat-0" data-multilevel-element>
+        Category 0
+      </button>
+      <button type="button" class="btn link" data-xt-group="cat-1" data-multilevel-element>
+        Category 1
+      </button>
+      <a href="#" class="btn link">
+        Link 0
+      </a>
+      <a href="#" class="btn link">
+        Link 1
+      </a>
     </nav>
 
-    <nav role="navigation" class="multilevel-tab" data-xt-group="cat-0">
-
-      <div class="multilevel-title-wrapper">
-        <div class="h4 multilevel-title">
-          Category 0
-        </div>
-        <button type="button" class="btn btn-none multilevel-reset">
-          Back
-        </button>
+    <div class="h5 flex items-center justify-between">
+      <div>
+        About
       </div>
-
-      <nav class="list-block multilevel-list">
-        <button type="button" class="btn" data-xt-group="cat-0a">
-          Category 0a
-        </button>
-        <button type="button" class="btn" data-xt-group="cat-0b">
-          Category 0b
-        </button>
-        <a href="#" class="btn">
-          Link 0
-        </a>
-        <a href="#" class="btn">
-          Link 1
-        </a>
-      </nav>
-
+    </div>
+    <nav class="list-block multilevel-list">
+      <a href="#" class="btn link">
+        Link 0
+      </a>
+      <a href="#" class="btn link">
+        Link 1
+      </a>
     </nav>
 
-    <nav role="navigation" class="multilevel-tab" data-xt-group="cat-0a">
+  </nav>
 
-      <div class="multilevel-title-wrapper">
-        <div class="h4 multilevel-title">
-          Category 0a
-        </div>
-        <button type="button" class="btn btn-none multilevel-reset">
-          Back
-        </button>
+  <nav role="navigation" class="toggle" data-multilevel-tab data-xt-group="cat-0">
+
+    <div class="h5 flex items-center justify-between">
+      <div>
+        Category 0
       </div>
+      <button type="button" class="btn link" data-multilevel-reset>
+        Back
+      </button>
+    </div>
 
-      <nav class="list-block multilevel-list">
-        <a href="#" class="btn">
-          Link 0
-        </a>
-        <a href="#" class="btn">
-          Link 1
-        </a>
-      </nav>
-
+    <nav class="list-block multilevel-list">
+      <button type="button" class="btn link" data-xt-group="cat-0a" data-multilevel-element>
+        Category 0a
+      </button>
+      <button type="button" class="btn link" data-xt-group="cat-0b" data-multilevel-element>
+        Category 0b
+      </button>
+      <a href="#" class="btn link">
+        Link 0
+      </a>
+      <a href="#" class="btn link">
+        Link 1
+      </a>
     </nav>
 
-    <nav role="navigation" class="multilevel-tab" data-xt-group="cat-0b">
+  </nav>
 
-      <div class="multilevel-title-wrapper">
-        <div class="h4 multilevel-title">
-          Category 0b
-        </div>
-        <button type="button" class="btn btn-none multilevel-reset">
-          Back
-        </button>
+  <nav role="navigation" class="toggle" data-multilevel-tab data-xt-group="cat-0a">
+
+    <div class="h5 flex items-center justify-between">
+      <div>
+        Category 0a
       </div>
+      <button type="button" class="btn link" data-multilevel-reset>
+        Back
+      </button>
+    </div>
 
-      <nav class="list-block multilevel-list">
-        <a href="#" class="btn">
-          Link 0
-        </a>
-        <a href="#" class="btn">
-          Link 1
-        </a>
-      </nav>
-
+    <nav class="list-block multilevel-list">
+      <a href="#" class="btn link">
+        Link 0
+      </a>
+      <a href="#" class="btn link">
+        Link 1
+      </a>
     </nav>
 
-    <nav role="navigation" class="multilevel-tab" data-xt-group="cat-1">
+  </nav>
 
-      <div class="multilevel-title-wrapper">
-        <div class="h4 multilevel-title">
-          Category 1
-        </div>
-        <button type="button" class="btn btn-none multilevel-reset">
-          Back
-        </button>
+  <nav role="navigation" class="toggle" data-multilevel-tab data-xt-group="cat-0b">
+
+    <div class="h5 flex items-center justify-between">
+      <div>
+        Category 0b
       </div>
+      <button type="button" class="btn link" data-multilevel-reset>
+        Back
+      </button>
+    </div>
 
-      <nav class="list-block multilevel-list">
-        <a href="#" class="btn">
-          Link 0
-        </a>
-        <a href="#" class="btn">
-          Link 1
-        </a>
-      </nav>
-
+    <nav class="list-block multilevel-list">
+      <a href="#" class="btn link">
+        Link 0
+      </a>
+      <a href="#" class="btn link">
+        Link 1
+      </a>
     </nav>
 
-  </div>
+  </nav>
+
+  <nav role="navigation" class="toggle" data-multilevel-tab data-xt-group="cat-1">
+
+    <div class="h5 flex items-center justify-between">
+      <div>
+        Category 1
+      </div>
+      <button type="button" class="btn link" data-multilevel-reset>
+        Back
+      </button>
+    </div>
+
+    <nav class="list-block multilevel-list">
+      <a href="#" class="btn link">
+        Link 0
+      </a>
+      <a href="#" class="btn link">
+        Link 1
+      </a>
+    </nav>
+
+  </nav>
+
 </div>
 `
--- src/private/demos/components/addons/toggle/multilevel.js
+++ src/private/demos/components/addons/toggle/multilevel.js
@@ -10,18 +10,18 @@ Xt.mount.push({
     // init
 
     let self = new Xt.Toggle(object, {
-      elements: '.multilevel-tab-reset, .multilevel-tab .multilevel-list button',
-      targets: '.multilevel-tab',
+      elements: '[data-multilevel-reset], [data-multilevel-element]',
+      targets: '[data-multilevel-tab]',
       min: 1,
     })
 
     // click
 
     const eventClick = () => {
-      object.querySelector('.multilevel-tab-reset').dispatchEvent(new CustomEvent('on.trigger.xt'))
+      object.querySelector('[data-multilevel-reset]').dispatchEvent(new CustomEvent('on.trigger.xt'))
     }
 
-    const btns = object.querySelectorAll('.multilevel-reset')
+    const btns = object.querySelectorAll('[data-multilevel-reset]')
     for (const btn of btns) {
       btn.addEventListener('click', eventClick)
     }
