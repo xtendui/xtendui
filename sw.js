6d3058bde463c53949be2618876154d147131921
minimit <beaver82@gmail.com>
2020-10-08 18:21:42 +0000

    Deploying to gh-pages from  @ c771e95ec5abab60b90e552e70ab846a9103ade4 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f1e7ee32414361438e9b.js"
+    "url": "webpack-runtime-769aabd980a4641cd0c2.js"
   },
   {
     "url": "framework-f3f6a2bb9db0f4de88ab.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a9ff4934d177e3f7f54e6a6e0f531383"
+    "revision": "c28ead32f61e012599d8e1aa33897a2f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c9875e2473a7c518755f.js"
