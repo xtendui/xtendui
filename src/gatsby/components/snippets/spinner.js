8ced0793748c7fcd5db4093d7a1ee0726bdb4619
Riccardo Caroli <rcaroli@webgriffe.com>
2020-10-13 17:41:42 +0200

    fix: bump tailwind and fix spinner transform

--- src/gatsby/components/snippets/spinner.js
+++ src/gatsby/components/snippets/spinner.js
@@ -1,6 +1,12 @@
 // const spinner = require('components/snippets/spinner').default
 // ${spinner({})}
 
-export default ({ width = 120, height = 120, radius = 100, dash = 628 }) => {
-  return `<svg viewBox="0 0 250 250"><circle cx="${width}" cy="${height}" r="${radius}" stroke-dasharray="${dash}" stroke-dashoffset="${dash}" pathLength="${dash}" class="stroke-current"/></svg><svg viewBox="0 0 250 250" preserveAspectRatio="xMinYMin meet"><circle cx="${width}" cy="${height}" r="${radius}" stroke-dasharray="${dash}" stroke-dashoffset="${dash}" pathLength="${dash}" class="stroke-current opacity-25"/></svg>`
+export default ({ dimension = 120, radius = 100, dash = 628 }) => {
+  return `<svg viewBox="0 0 ${dimension * 2} ${
+    dimension * 2
+  }"><circle cx="${dimension}" cy="${dimension}" r="${radius}" stroke-dasharray="${dash}" stroke-dashoffset="${dash}" pathLength="${dash}" class="stroke-current"/></svg><svg viewBox="0 0 ${
+    dimension * 2
+  } ${
+    dimension * 2
+  }" preserveAspectRatio="xMinYMin meet"><circle cx="${dimension}" cy="${dimension}" r="${radius}" stroke-dasharray="${dash}" stroke-dashoffset="${dash}" pathLength="${dash}" class="stroke-current opacity-25"/></svg>`
 }
