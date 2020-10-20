4e7211e0d43aaabf8ab4d499e8bec114de5aa91f
Riccardo Caroli <rcaroli@webgriffe.com>
2020-10-20 10:55:03 +0200

    chore: shadow revision

--- src/gatsby/components/snippets/classes/card-default-full-interactive.js
+++ src/gatsby/components/snippets/classes/card-default-full-interactive.js
@@ -1,3 +1,3 @@
 export default () => {
-  return `text-default rounded-md transform hover:bg-gray-200 hover:shadow-2xl hover:-translate-y-2 active:bg-gray-300 active:shadow-none active:translate-y-0`
+  return `text-default rounded-md transform hover:bg-gray-200 hover:shadow-xl hover:-translate-y-2 active:bg-gray-300 active:shadow-md active:-translate-y-1`
 }
--- src/gatsby/components/snippets/classes/card-primary-full-interactive.js
+++ src/gatsby/components/snippets/classes/card-primary-full-interactive.js
@@ -1,3 +1,3 @@
 export default () => {
-  return `text-default rounded-md transform hover:text-inverse hover:bg-accent-500 hover:shadow-2xl hover:-translate-y-2 active:text-inverse active:bg-accent-600 active:shadow-none active:translate-y-0`
+  return `text-default rounded-md transform hover:text-inverse hover:bg-accent-500 hover:shadow-xl hover:-translate-y-2 active:text-inverse active:bg-accent-600 active:shadow-md active:-translate-y-1`
 }
