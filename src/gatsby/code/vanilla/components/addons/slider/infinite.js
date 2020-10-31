c49d19c745a95df2abb87b667dc6670b81141029
minimit <beaver82@gmail.com>
2020-10-31 15:47:51 +0100

    chore: slider infinite classes

--- src/gatsby/code/vanilla/components/addons/slider/infinite.js
+++ src/gatsby/code/vanilla/components/addons/slider/infinite.js
@@ -21,45 +21,43 @@ demo.htmlSource = `
     <div class="slides">
       <ul class="slides-inner row row-10 transition-none">
 
-        <div class="slide">
-          <div class="brand list list-10 flex-no-wrap whitespace-nowrap">
-            <a href="#test-link-1" class="brand_content">
-              <div class="h4">One</div>
-            </a>
-            <a href="#test-link-2" class="brand_content">
-              <div class="h4">Two</div>
-            </a>
-            <a href="#test-link-3" class="brand_content">
-              <div class="h4">Three</div>
-            </a>
-            <a href="#test-link-4" class="brand_content">
-              <div class="h4">Four</div>
-            </a>
-            <a href="#test-link-5" class="brand_content">
-              <div class="h4">Five</div>
-            </a>
-            <a href="#test-link-6" class="brand_content">
-              <div class="h4">Six</div>
-            </a>
-            <a href="#test-link-7" class="brand_content">
-              <div class="h4">Seven</div>
-            </a>
-            <a href="#test-link-8" class="brand_content">
-              <div class="h4">Eight</div>
-            </a>
-            <a href="#test-link-9" class="brand_content">
-              <div class="h4">Nine</div>
-            </a>
-            <a href="#test-link-10" class="brand_content">
-              <div class="h4">Ten</div>
-            </a>
-            <a href="#test-link-11" class="brand_content">
-              <div class="h4">Eleven</div>
-            </a>
-            <a href="#test-link-12" class="brand_content">
-              <div class="h4">Twelve</div>
-            </a>
-          </div>
+        <div class="slide list list-10 flex-no-wrap whitespace-nowrap">
+          <a href="#test-link-1" class="slide_item" title="item 1">
+            <div class="h4">One</div>
+          </a>
+          <a href="#test-link-2" class="slide_item" title="item 2">
+            <div class="h4">Two</div>
+          </a>
+          <a href="#test-link-3" class="slide_item" title="item 3">
+            <div class="h4">Three</div>
+          </a>
+          <a href="#test-link-4" class="slide_item" title="item 4">
+            <div class="h4">Four</div>
+          </a>
+          <a href="#test-link-5" class="slide_item" title="item 5">
+            <div class="h4">Five</div>
+          </a>
+          <a href="#test-link-6" class="slide_item" title="item 6">
+            <div class="h4">Six</div>
+          </a>
+          <a href="#test-link-7" class="slide_item" title="item 7">
+            <div class="h4">Seven</div>
+          </a>
+          <a href="#test-link-8" class="slide_item" title="item 8">
+            <div class="h4">Eight</div>
+          </a>
+          <a href="#test-link-9" class="slide_item" title="item 9">
+            <div class="h4">Nine</div>
+          </a>
+          <a href="#test-link-10" class="slide_item" title="item 10">
+            <div class="h4">Ten</div>
+          </a>
+          <a href="#test-link-11" class="slide_item" title="item 11">
+            <div class="h4">Eleven</div>
+          </a>
+          <a href="#test-link-12" class="slide_item" title="item 12">
+            <div class="h4">Twelve</div>
+          </a>
         </div>
 
       </ul>
@@ -81,45 +79,43 @@ demo.htmlSource = `
     <div class="slides">
       <ul class="slides-inner row row-10 transition-none">
 
-        <div class="slide">
-          <div class="brand list list-10 flex-no-wrap whitespace-nowrap">
-            <a href="#test-link-1" class="brand_content">
-              <div class="h4">One</div>
-            </a>
-            <a href="#test-link-2" class="brand_content">
-              <div class="h4">Two</div>
-            </a>
-            <a href="#test-link-3" class="brand_content">
-              <div class="h4">Three</div>
-            </a>
-            <a href="#test-link-4" class="brand_content">
-              <div class="h4">Four</div>
-            </a>
-            <a href="#test-link-5" class="brand_content">
-              <div class="h4">Five</div>
-            </a>
-            <a href="#test-link-6" class="brand_content">
-              <div class="h4">Six</div>
-            </a>
-            <a href="#test-link-7" class="brand_content">
-              <div class="h4">Seven</div>
-            </a>
-            <a href="#test-link-8" class="brand_content">
-              <div class="h4">Eight</div>
-            </a>
-            <a href="#test-link-9" class="brand_content">
-              <div class="h4">Nine</div>
-            </a>
-            <a href="#test-link-10" class="brand_content">
-              <div class="h4">Ten</div>
-            </a>
-            <a href="#test-link-11" class="brand_content">
-              <div class="h4">Eleven</div>
-            </a>
-            <a href="#test-link-12" class="brand_content">
-              <div class="h4">Twelve</div>
-            </a>
-          </div>
+        <div class="slide list list-10 flex-no-wrap whitespace-nowrap">
+          <a href="#test-link-1" class="slide_item" title="item 1">
+            <div class="h4">One</div>
+          </a>
+          <a href="#test-link-2" class="slide_item" title="item 2">
+            <div class="h4">Two</div>
+          </a>
+          <a href="#test-link-3" class="slide_item" title="item 3">
+            <div class="h4">Three</div>
+          </a>
+          <a href="#test-link-4" class="slide_item" title="item 4">
+            <div class="h4">Four</div>
+          </a>
+          <a href="#test-link-5" class="slide_item" title="item 5">
+            <div class="h4">Five</div>
+          </a>
+          <a href="#test-link-6" class="slide_item" title="item 6">
+            <div class="h4">Six</div>
+          </a>
+          <a href="#test-link-7" class="slide_item" title="item 7">
+            <div class="h4">Seven</div>
+          </a>
+          <a href="#test-link-8" class="slide_item" title="item 8">
+            <div class="h4">Eight</div>
+          </a>
+          <a href="#test-link-9" class="slide_item" title="item 9">
+            <div class="h4">Nine</div>
+          </a>
+          <a href="#test-link-10" class="slide_item" title="item 10">
+            <div class="h4">Ten</div>
+          </a>
+          <a href="#test-link-11" class="slide_item" title="item 11">
+            <div class="h4">Eleven</div>
+          </a>
+          <a href="#test-link-12" class="slide_item" title="item 12">
+            <div class="h4">Twelve</div>
+          </a>
         </div>
 
       </ul>
@@ -141,27 +137,25 @@ demo.htmlSource = `
     <div class="slides">
       <ul class="slides-inner row row-px transition-none">
 
-        <div class="slide">
-          <div class="brand list list-px flex-no-wrap">
-            <a href="#test-link-1" class="brand_content">
-              ${img({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
-            </a>
-            <a href="#test-link-2" class="brand_content">
-              ${img({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
-            </a>
-            <a href="#test-link-3" class="brand_content">
-              ${img({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
-            </a>
-            <a href="#test-link-4" class="brand_content">
-              ${img({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
-            </a>
-            <a href="#test-link-5" class="brand_content">
-              ${img({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
-            </a>
-            <a href="#test-link-6" class="brand_content">
-              ${imgAlt({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
-            </a>
-          </div>
+        <div class="slide list list-px flex-no-wrap">
+          <a href="#test-link-1" class="slide_item" title="item 0">
+            ${img({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
+          </a>
+          <a href="#test-link-2" class="slide_item" title="item 1">
+            ${img({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
+          </a>
+          <a href="#test-link-3" class="slide_item" title="item 2">
+            ${img({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
+          </a>
+          <a href="#test-link-4" class="slide_item" title="item 3">
+            ${img({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
+          </a>
+          <a href="#test-link-5" class="slide_item" title="item 4">
+            ${img({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
+          </a>
+          <a href="#test-link-6" class="slide_item" title="item 5">
+            ${imgAlt({ classes: 'object-cover object-center', ratio: '37.5%', loading: 'eager' })}
+          </a>
         </div>
 
       </ul>
