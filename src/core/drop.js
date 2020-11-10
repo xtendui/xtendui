a749c433c896853f1dd14af396aec6c80fc2a459
minimit <beaver82@gmail.com>
2020-11-10 13:06:43 +0100

    chore: test fix popperjs

--- src/core/drop.js
+++ src/core/drop.js
@@ -110,15 +110,14 @@ class Drop extends Xt.Toggle {
    * @param {Node|HTMLElement|EventTarget|Window} el Elements to be deactivated
    * @param {String} type Type of elements
    */
-  deactivate(el, type) {
+  deactivateDone(el, type) {
     const self = this
     // super
-    super.deactivate(el)
+    super.deactivateDone(el)
     // popper
     if (type === 'targets') {
       const popperInstance = Xt.dataStorage.get(el, `${self.componentNamespace}Popper`)
       if (popperInstance) {
-        console.log(el)
         popperInstance.destroy()
         Xt.dataStorage.remove(el, `${self.componentNamespace}Popper`)
       }
