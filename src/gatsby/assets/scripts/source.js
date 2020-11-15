978c3af7a532def0f4a12f0540c35aa4358679f8
minimit <beaver82@gmail.com>
2020-11-16 00:48:49 +0100

    chore: [skip ci]

--- src/gatsby/assets/scripts/source.js
+++ src/gatsby/assets/scripts/source.js
@@ -1,12 +1,12 @@
-const cssSource = (demo, nameOverride, type = '.css') => {
+const cssSource = (demo, type = '.css') => {
   return require(`!!raw-loader!xtendui/src/${demo.path}${type}`).default
 }
 
-const jsSource = (demo, nameOverride, type = '.js') => {
+const jsSource = (demo, type = '.js') => {
   return require(`!!raw-loader!xtendui/src/${demo.path}${type}`).default
 }
 
-const jsxSource = (demo, nameOverride, type = '.jsx') => {
+const jsxSource = (demo, type = '.jsx') => {
   return require(`!!raw-loader!xtendui/src/${demo.type}${type}`).default
 }
 
