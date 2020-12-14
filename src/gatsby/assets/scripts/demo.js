f75181f2ed08b597d61fb5555cbcc6d8ef16663e
Riccardo Caroli <rcaroli@webgriffe.com>
2020-12-14 10:30:51 +0100

    chore: fix unescape codeql

--- src/gatsby/assets/scripts/demo.js
+++ src/gatsby/assets/scripts/demo.js
@@ -89,14 +89,17 @@ const formatCode = (source, sourceCode) => {
     text = text.replace(new RegExp(`[ ]{0,}${id}[ ]{0,}`, 'gi'), '')
   }
   // clipboard
-  text = text
-    .replace(/&amp;/g, '&')
-    .replace(/&lt;/g, '<')
-    .replace(/&gt;/g, '>')
-    .replace(/&quot;/g, '"')
-    .replace(/&#39;/g, "'")
-    .replace(/&#x2F;/g, '/')
-  Xt.dataStorage.set(sourceCode, 'sourceCode', text)
+  Xt.dataStorage.set(
+    sourceCode,
+    'sourceCode',
+    text
+      .replace(/&amp;/g, '&')
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>')
+      .replace(/&quot;/g, '"')
+      .replace(/&#39;/g, "'")
+      .replace(/&#x2F;/g, '/')
+  )
   // search html tags
   const re = /<[^>]*>/g
   text = text.replace(re, match => {
