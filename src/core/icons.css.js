5d7f6540fd5dff329fe0086c5bd91b9c400b72b3
minimit <beaver82@gmail.com>
2021-01-04 22:49:01 +0100

    fix(css): fix icons negative margins

--- src/core/icons.css.js
+++ src/core/icons.css.js
@@ -11,16 +11,19 @@ module.exports = {
       // styles
       width: '1em',
       height: '1em',
+      margin: '0px', // negative margin
     },
     '.icon-md': {
       // styles
       width: '1.25em',
       height: '1.25em',
+      margin: 'calc((1em - 1.25em) / 2)', // negative margin
     },
     '.icon-lg': {
       // styles
       width: '1.5em',
       height: '1.5em',
+      margin: 'calc((1em - 1.5em) / 2)', // negative margin
     },
   },
 }
