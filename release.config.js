5ca29984f0544fe0ba3eb5c4f7e0f6150fdd9632
Riccardo Caroli <rcaroli@webgriffe.com>
2021-01-11 17:23:31 +0100

    chore: test release beta

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   debug: true,
   repositoryUrl: 'https://github.com/minimit/xtendui',
-  branches: ['master', { name: '1.0-stable', prerelease: false, range: '1.x.x' }, { name: 'beta', prerelease: true }],
+  branches: ['master', { name: 'beta', prerelease: true }, { name: '1.0-stable', prerelease: false, range: '1.x.x' }],
   plugins: [
     ['@semantic-release/commit-analyzer'],
     ['@semantic-release/release-notes-generator'],
