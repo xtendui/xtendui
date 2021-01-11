d0eb3b89914be2101b6ca4659494f13fef2b7fee
Riccardo Caroli <rcaroli@webgriffe.com>
2021-01-11 14:40:08 +0100

    release: only master

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   debug: true,
   repositoryUrl: 'https://github.com/minimit/xtendui',
-  branches: ['master', { name: '0.16', prerelease: false, range: '0.16.x' }, { name: 'beta', prerelease: true }, { name: 'alpha', prerelease: true }],
+  branches: ['master'],
   plugins: [
     ['@semantic-release/commit-analyzer'],
     ['@semantic-release/release-notes-generator'],
