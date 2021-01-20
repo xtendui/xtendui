f2d95a5f27bd8f8e322f76d1af66c7b8d8cf0654
Riccardo Caroli <rcaroli@webgriffe.com>
2021-01-20 12:47:04 +0100

    chore: docs do not hide sticky header

--- src/gatsby/assets/scripts/app.js
+++ src/gatsby/assets/scripts/app.js
@@ -50,41 +50,6 @@ Xt.mount.push({
           end: 'bottom top',
           pin: true,
           pinSpacing: false,
-          onUpdate: self => {
-            // scrolling-down depending on scroll direction
-            if (!self.getVelocity()) return // skip on initial
-            if (object.classList.contains('scrolling-hide') && self.direction < 0) {
-              object.classList.remove('scrolling-down')
-              gsap.killTweensOf(object)
-              gsap.to(object, { y: 0, duration: 0.5, ease: 'quart.out' })
-            } else if (object.classList.contains('scrolling-hide') && self.direction > 0) {
-              object.classList.add('scrolling-down')
-              gsap.killTweensOf(object)
-              gsap.to(object, {
-                y: -object.offsetHeight,
-                duration: 0.5,
-                ease: 'quart.out',
-              })
-            }
-          },
-        })
-
-        // hide depending on .gatsby_site-article_hero
-
-        ScrollTrigger.create({
-          trigger: object,
-          start: -1, // needs -1 because start trigger is sticky
-          endTrigger: document.querySelector('.gatsby_site-article_hero'),
-          end: `bottom top`,
-          onUpdate: self => {
-            if (self.isActive && object.classList.contains('scrolling-hide')) {
-              object.classList.remove('scrolling-hide')
-              gsap.killTweensOf(object)
-              gsap.to(object, { y: 0, duration: 0.5, ease: 'quart.out' })
-            } else if (!self.isActive && !object.classList.contains('scrolling-hide')) {
-              object.classList.add('scrolling-hide')
-            }
-          },
         })
       },
     })
