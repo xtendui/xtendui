1f77137720d19ae832f51d1c31d13972915845b7
Riccardo Caroli <rcaroli@webgriffe.com>
2021-02-17 14:57:36 +0100

    chore: lint

--- src/gatsby/components/includes/doc-fullscreen.js
+++ src/gatsby/components/includes/doc-fullscreen.js
@@ -7,7 +7,11 @@ export default class DocFullscreen extends React.Component {
       <>
         <div id="gatsby_open-full-trigger"></div>
         <div id="gatsby_open-full" className="xt-toggle">
-          <div className="xt-button xt-dismiss absolute z-last top-0 right-0" aria-label="Close" dangerouslySetInnerHTML={{ __html: iconX() }}></div>
+          <div
+            className="xt-button xt-dismiss absolute z-last top-0 right-0"
+            aria-label="Close"
+            dangerouslySetInnerHTML={{ __html: iconX() }}
+          ></div>
           <div className="xt-card-content" id="gatsby_open-full-content"></div>
         </div>
       </>
