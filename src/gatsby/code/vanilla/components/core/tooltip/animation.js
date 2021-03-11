4eb9e0a1b73fd9d7203640659a6c6a479d5775d6
Riccardo Caroli <rcaroli@webgriffe.com>
2021-03-11 18:24:28 +0100

    chore: lint

--- src/gatsby/code/vanilla/components/core/tooltip/animation.js
+++ src/gatsby/code/vanilla/components/core/tooltip/animation.js
@@ -1,7 +1,8 @@
 import path from 'path'
 
 const classes = require('components/snippets/classes').classes
-ilename = __filename.replace(/\\/g, '/')
+
+const filename = __filename.replace(/\\/g, '/')
 const dirs = path.dirname(filename).split('/')
 export const demo = {
   name: path.basename(filename, '.js'),
