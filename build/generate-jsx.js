113f9dd8d15afb004b4e967411e5a1ae6a1a3bf1
minimit <beaver82@gmail.com>
2021-03-13 13:15:54 +0100

    docs: refactor demos with classes and html in static and react demos generation

--- build/generate-jsx.js
+++ build/generate-jsx.js
@@ -5,25 +5,27 @@ const glob = require('glob')
 const writeFile = require('write')
 const indentString = require('indent-string')
 
-new glob.Glob('static/**/**.html', (er, files) => {
-  for (const file of files) {
-    const name = path.basename(file, '.html')
-    const dir = path.dirname(file)
-    const src = dir + '/' + name
-    let str = `import React from 'react'
+;(async () => {
+  await del(['static/**/**.jsx'])
+  new glob.Glob('static/**/**.html', (er, files) => {
+    for (const file of files) {
+      const name = path.basename(file, '.html')
+      const dir = path.dirname(file)
+      const src = dir + '/' + name
+      let str = `import React from 'react'
 `
-    const jsGlob = new glob.Glob(`${src}.js`, (er, jsSources) => {
-      for (const jsSource of jsSources) {
-        try {
-          const jsText = fs.readFileSync(jsSource, 'utf8')
-          // ##IMPORTSSTART and ##IMPORTSEND
-          const imports = jsText.match(/\/\*##IMPORTSSTART\*\/\n([\S\s]*?)\/\*##IMPORTSEND\*\/\n/g)
-          if (imports) {
-            for (const meta of imports.entries()) {
-              str += meta[1].replace(/\/\*##IMPORTSSTART\*\/\n/g, '').replace(/\/\*##IMPORTSEND\*\/\n/g, '')
+      const jsGlob = new glob.Glob(`${src}.js`, (er, jsSources) => {
+        for (const jsSource of jsSources) {
+          try {
+            const jsText = fs.readFileSync(jsSource, 'utf8')
+            // ##IMPORTSSTART and ##IMPORTSEND
+            const imports = jsText.match(/\/\*##IMPORTSSTART\*\/\n([\S\s]*?)\/\*##IMPORTSEND\*\/\n/g)
+            if (imports) {
+              for (const meta of imports.entries()) {
+                str += meta[1].replace(/\/\*##IMPORTSSTART\*\/\n/g, '').replace(/\/\*##IMPORTSEND\*\/\n/g, '')
+              }
             }
-          }
-          str += `
+            str += `
 class Demo extends React.Component {
   constructor(props) {
     super(props)
@@ -33,64 +35,65 @@ class Demo extends React.Component {
   componentDidMount() {
     this.object = this.ref.current
 `
-          // ##COMPONENTDIDMOUNTSTART and ##COMPONENTDIDMOUNTEND
-          const mounts = jsText.match(
-            /[ ]*\/\*##COMPONENTDIDMOUNTSTART\*\/\n([\S\s]*?)[ ]*\/\*##COMPONENTDIDMOUNTEND\*\/\n/g
-          )
-          if (mounts) {
-            for (const meta of mounts.entries()) {
-              str += meta[1]
-                .replace(/[ ]*\/\*##COMPONENTDIDMOUNTSTART\*\/\n/g, '')
-                .replace(/[ ]*\/\*##COMPONENTDIDMOUNTEND\*\/\n/g, '')
+            // ##COMPONENTDIDMOUNTSTART and ##COMPONENTDIDMOUNTEND
+            const mounts = jsText.match(
+              /[ ]*\/\*##COMPONENTDIDMOUNTSTART\*\/\n([\S\s]*?)[ ]*\/\*##COMPONENTDIDMOUNTEND\*\/\n/g
+            )
+            if (mounts) {
+              for (const meta of mounts.entries()) {
+                str += meta[1]
+                  .replace(/[ ]*\/\*##COMPONENTDIDMOUNTSTART\*\/\n/g, '')
+                  .replace(/[ ]*\/\*##COMPONENTDIDMOUNTEND\*\/\n/g, '')
+              }
             }
-          }
-          str += `  }
+            str += `  }
 
   componentWillUnmount() {
 `
-          // ##COMPONENTDIDUNMOUNTSTART and ##COMPONENTDIDUNMOUNTEND
-          const unmount = jsText.match(
-            /[ ]*\/\*##COMPONENTDIDUNMOUNTSTART\*\/\n([\S\s]*?)[ ]*\/\*##COMPONENTDIDUNMOUNTEND\*\/\n/g
-          )
-          if (unmount) {
-            for (const meta of unmount.entries()) {
-              str += meta[1]
-                .replace(/[ ]*\/\*##COMPONENTDIDUNMOUNTSTART\*\/\n/g, '')
-                .replace(/[ ]*\/\*##COMPONENTDIDUNMOUNTEND\*\/\n/g, '')
+            // ##COMPONENTDIDUNMOUNTSTART and ##COMPONENTDIDUNMOUNTEND
+            const unmount = jsText.match(
+              /[ ]*\/\*##COMPONENTDIDUNMOUNTSTART\*\/\n([\S\s]*?)[ ]*\/\*##COMPONENTDIDUNMOUNTEND\*\/\n/g
+            )
+            if (unmount) {
+              for (const meta of unmount.entries()) {
+                str += meta[1]
+                  .replace(/[ ]*\/\*##COMPONENTDIDUNMOUNTSTART\*\/\n/g, '')
+                  .replace(/[ ]*\/\*##COMPONENTDIDUNMOUNTEND\*\/\n/g, '')
+              }
             }
-          }
-          str += `  }
+            str += `  }
 
 `
-          // ##METHODSSTART and ##METHODSEND
-          const methods = jsText.match(/[ ]*\/\*##METHODSSTART\*\/\n([\S\s]*?)[ ]*\/\*##METHODSEND\*\/\n/g)
-          if (methods) {
-            for (const meta of methods.entries()) {
-              str += meta[1].replace(/[ ]*\/\*##METHODSSTART\*\/\n/g, '').replace(/[ ]*\/\*##METHODSEND\*\/\n/g, '')
+            // ##METHODSSTART and ##METHODSEND
+            const methods = jsText.match(/[ ]*\/\*##METHODSSTART\*\/\n([\S\s]*?)[ ]*\/\*##METHODSEND\*\/\n/g)
+            if (methods) {
+              for (const meta of methods.entries()) {
+                str += meta[1].replace(/[ ]*\/\*##METHODSSTART\*\/\n/g, '').replace(/[ ]*\/\*##METHODSEND\*\/\n/g, '')
+              }
             }
-          }
-          str += `
+            str += `
   render() {
     return (
       <div ref={this.ref}>
 `
-        } catch (err) {
-          console.error(err)
+          } catch (err) {
+            console.error(err)
+          }
         }
-      }
-    })
-    jsGlob.on('end', () => {
-      const html = require('esm')(module)(path.resolve(`${src}.html`)).default
-      str += indentString(
-        html
-          .replace(/^export default const html(.*)\n/gm, '')
-          .replace(/class="/g, 'className="')
-          .split('\n')
-          .slice(2, -2)
-          .join('\n'),
-        6
-      )
-      str += `
+      })
+      jsGlob.on('end', () => {
+        module.resolve = path.resolve(`./src/gatsby/`) + '/'
+        const html = require('esm')(module)(path.resolve(`${src}.html`)).default
+        str += indentString(
+          html
+            .replace(/^export default const html(.*)\n/gm, '')
+            .replace(/class="/g, 'className="')
+            .split('\n')
+            .slice(2, -2)
+            .join('\n'),
+          6
+        )
+        str += `
       </div>
     )
   }
@@ -98,9 +101,10 @@ class Demo extends React.Component {
 
 export default Demo
 `
-      const destination = `${src}.jsx`
-      console.log('\nGenerating React', destination)
-      writeFile(destination, str)
-    })
-  }
-})
+        const destination = `${src}.jsx`
+        console.log('Generating React', destination)
+        writeFile(destination, str)
+      })
+    }
+  })
+})();
