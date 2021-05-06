9b77e04c9e5540f389640a2c43b5c7611847a3a3
Riccardo Caroli <rcaroli@webgriffe.com>
2021-05-06 18:16:02 +0200

    docs: body class iframes

--- src/gatsby/templates/layout-demo.js
+++ src/gatsby/templates/layout-demo.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
 
 import 'src/gatsby/assets/styles/app.css'
 
 export default class Layout extends React.Component {
   render() {
     const { children } = this.props
-    return <>{children}</>
+    return (
+      <>
+        <Helmet>
+          <body className="xt-body" />
+        </Helmet>
+        {children}
+      </>
+    )
   }
 }
 
