6415fcff9b65a0f710d6277b418a072850ff252d
minimit <beaver82@gmail.com>
2021-05-12 20:22:45 +0200

    docs: overlay fix multiple overlay inner and revision animations

--- static/demos/components/overlay/animation-js.html.js
+++ static/demos/components/overlay/animation-js.html.js
@@ -31,59 +31,57 @@ const html = /* HTML */ `
         <div class="xt-backdrop bg-black pointer-events-none transition opacity-0 group-in:opacity-25"></div>
         <div class="xt-overlay-container max-w-3xl">
           <div class="xt-overlay-inner">
-            <div class="xt-overlay-inner">
-              <div class="xt-card rounded-md shadow-overlay ${classes.cardWhite()}">
-                <button
-                  type="button"
-                  class="xt-button xt-dismiss absolute z-last top-0 right-0 p-5 text-2xl"
-                  aria-label="Close"
-                >
-                  ${classes.iconX()}
-                </button>
-                <div class="${classes.cardMd()}">
-                  <div class="xt-h4">Overlay 0</div>
-                  <p>
-                    <strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam
-                    suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris
-                    augue, sagittis vitae magna eget, vehicula scelerisque elit.
-                  </p>
-                  <p>
-                    Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
-                    justo.
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
-                    ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.
-                  </p>
-                  <p>
-                    Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
-                    justo.
-                  </p>
-                  <div class="xt-list xt-list-3 items-center">
-                    <button
-                      type="button"
-                      class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
-                      data-xt-group="group0"
-                    >
-                      overlay 0
-                    </button>
+            <div class="xt-card rounded-md shadow-overlay ${classes.cardWhite()}">
+              <button
+                type="button"
+                class="xt-button xt-dismiss absolute z-last top-0 right-0 p-5 text-2xl"
+                aria-label="Close"
+              >
+                ${classes.iconX()}
+              </button>
+              <div class="${classes.cardMd()}">
+                <div class="xt-h4">Overlay 0</div>
+                <p>
+                  <strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam
+                  suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris
+                  augue, sagittis vitae magna eget, vehicula scelerisque elit.
+                </p>
+                <p>
+                  Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                  justo.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
+                  ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.
+                </p>
+                <p>
+                  Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                  justo.
+                </p>
+                <div class="xt-list xt-list-3 items-center">
+                  <button
+                    type="button"
+                    class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
+                    data-xt-group="group0"
+                  >
+                    overlay 0
+                  </button>
 
-                    <button
-                      type="button"
-                      class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
-                      data-xt-group="group1"
-                    >
-                      overlay 1
-                    </button>
+                  <button
+                    type="button"
+                    class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
+                    data-xt-group="group1"
+                  >
+                    overlay 1
+                  </button>
 
-                    <button
-                      type="button"
-                      class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
-                      data-xt-group="group2"
-                    >
-                      overlay 2
-                    </button>
-                  </div>
+                  <button
+                    type="button"
+                    class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
+                    data-xt-group="group2"
+                  >
+                    overlay 2
+                  </button>
                 </div>
               </div>
             </div>
@@ -95,59 +93,57 @@ const html = /* HTML */ `
         <div class="xt-backdrop bg-black pointer-events-none transition opacity-0 group-in:opacity-25"></div>
         <div class="xt-overlay-container max-w-3xl">
           <div class="xt-overlay-inner">
-            <div class="xt-overlay-inner">
-              <div class="xt-card rounded-md shadow-overlay ${classes.cardWhite()}">
-                <button
-                  type="button"
-                  class="xt-button xt-dismiss absolute z-last top-0 right-0 p-5 text-2xl"
-                  aria-label="Close"
-                >
-                  ${classes.iconX()}
-                </button>
-                <div class="${classes.cardMd()}">
-                  <div class="xt-h4">Overlay 1</div>
-                  <p>
-                    <strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam
-                    suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris
-                    augue, sagittis vitae magna eget, vehicula scelerisque elit.
-                  </p>
-                  <p>
-                    Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
-                    justo.
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
-                    ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.
-                  </p>
-                  <p>
-                    Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
-                    justo.
-                  </p>
-                  <div class="xt-list xt-list-3 items-center">
-                    <button
-                      type="button"
-                      class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
-                      data-xt-group="group0"
-                    >
-                      overlay 0
-                    </button>
+            <div class="xt-card rounded-md shadow-overlay ${classes.cardWhite()}">
+              <button
+                type="button"
+                class="xt-button xt-dismiss absolute z-last top-0 right-0 p-5 text-2xl"
+                aria-label="Close"
+              >
+                ${classes.iconX()}
+              </button>
+              <div class="${classes.cardMd()}">
+                <div class="xt-h4">Overlay 1</div>
+                <p>
+                  <strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam
+                  suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris
+                  augue, sagittis vitae magna eget, vehicula scelerisque elit.
+                </p>
+                <p>
+                  Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                  justo.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
+                  ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.
+                </p>
+                <p>
+                  Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                  justo.
+                </p>
+                <div class="xt-list xt-list-3 items-center">
+                  <button
+                    type="button"
+                    class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
+                    data-xt-group="group0"
+                  >
+                    overlay 0
+                  </button>
 
-                    <button
-                      type="button"
-                      class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
-                      data-xt-group="group1"
-                    >
-                      overlay 1
-                    </button>
+                  <button
+                    type="button"
+                    class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
+                    data-xt-group="group1"
+                  >
+                    overlay 1
+                  </button>
 
-                    <button
-                      type="button"
-                      class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
-                      data-xt-group="group2"
-                    >
-                      overlay 2
-                    </button>
-                  </div>
+                  <button
+                    type="button"
+                    class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
+                    data-xt-group="group2"
+                  >
+                    overlay 2
+                  </button>
                 </div>
               </div>
             </div>
@@ -159,59 +155,57 @@ const html = /* HTML */ `
         <div class="xt-backdrop bg-black pointer-events-none transition opacity-0 group-in:opacity-25"></div>
         <div class="xt-overlay-container max-w-3xl">
           <div class="xt-overlay-inner">
-            <div class="xt-overlay-inner">
-              <div class="xt-card rounded-md shadow-overlay ${classes.cardWhite()}">
-                <button
-                  type="button"
-                  class="xt-button xt-dismiss absolute z-last top-0 right-0 p-5 text-2xl"
-                  aria-label="Close"
-                >
-                  ${classes.iconX()}
-                </button>
-                <div class="${classes.cardMd()}">
-                  <div class="xt-h4">Overlay 2</div>
-                  <p>
-                    <strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam
-                    suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris
-                    augue, sagittis vitae magna eget, vehicula scelerisque elit.
-                  </p>
-                  <p>
-                    Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
-                    justo.
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
-                    ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.
-                  </p>
-                  <p>
-                    Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
-                    justo.
-                  </p>
-                  <div class="xt-list xt-list-3 items-center">
-                    <button
-                      type="button"
-                      class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
-                      data-xt-group="group0"
-                    >
-                      overlay 0
-                    </button>
+            <div class="xt-card rounded-md shadow-overlay ${classes.cardWhite()}">
+              <button
+                type="button"
+                class="xt-button xt-dismiss absolute z-last top-0 right-0 p-5 text-2xl"
+                aria-label="Close"
+              >
+                ${classes.iconX()}
+              </button>
+              <div class="${classes.cardMd()}">
+                <div class="xt-h4">Overlay 2</div>
+                <p>
+                  <strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam
+                  suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris
+                  augue, sagittis vitae magna eget, vehicula scelerisque elit.
+                </p>
+                <p>
+                  Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                  justo.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus, lectus quis ornare volutpat,
+                  ligula nulla sollicitudin nunc, ut commodo nulla enim nec nisi.
+                </p>
+                <p>
+                  Morbi sodales, dolor a iaculis ornare, velit justo lacinia erat, pretium sollicitudin dui sem id
+                  justo.
+                </p>
+                <div class="xt-list xt-list-3 items-center">
+                  <button
+                    type="button"
+                    class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
+                    data-xt-group="group0"
+                  >
+                    overlay 0
+                  </button>
 
-                    <button
-                      type="button"
-                      class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
-                      data-xt-group="group1"
-                    >
-                      overlay 1
-                    </button>
+                  <button
+                    type="button"
+                    class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
+                    data-xt-group="group1"
+                  >
+                    overlay 1
+                  </button>
 
-                    <button
-                      type="button"
-                      class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
-                      data-xt-group="group2"
-                    >
-                      overlay 2
-                    </button>
-                  </div>
+                  <button
+                    type="button"
+                    class="xt-button ${classes.buttonMd()} rounded-md ${classes.buttonPrimary()}"
+                    data-xt-group="group2"
+                  >
+                    overlay 2
+                  </button>
                 </div>
               </div>
             </div>
