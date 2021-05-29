99c13c9863911a158739c4847a8a406ec701d0f9
minimit <beaver82@gmail.com>
2021-05-29 13:47:56 +0200

    docs: drop static multiple

--- static/demos/components/drop/static.html.js
+++ static/demos/components/drop/static.html.js
@@ -3,42 +3,78 @@ const classes = require('src/gatsby/templates/snippets/classes').classes
 const html = /* HTML */ `
   <div>
     <div class="relative py-4 px-6 bg-primary-500">
-      <div data-xt-drop="{ elements: ':scope > .xt-button', targets: ':scope > .xt-drop' }">
-        <button
-          type="button"
-          class="xt-button ${classes.buttonMd()} ${classes.buttonRadius()} ${classes.buttonPrimary()}"
-        >
-          static
-        </button>
+      <div
+        class="xt-list xt-list-3 items-center"
+        data-xt-drop="{ elements: ':scope > .xt-drop-item', targets: ':scope > .xt-drop-item > .xt-drop' }"
+      >
+        <div class="xt-drop-item">
+          <button
+            type="button"
+            class="xt-button ${classes.buttonMd()} ${classes.buttonRadius()} ${classes.buttonPrimary()}"
+          >
+            Drop
+          </button>
 
-        <div class="xt-drop xt-drop-static">
-          <div class="xt-card w-full ${classes.cardRadius()} shadow-drop ${classes.cardWhite()}">
-            <div class="md:xt-card-group">
-              <div class="${classes.cardMd()} md:w-7/12 bg-gray-300">
-                <div class="xt-h4">Lorem ipsum</div>
+          <div class="xt-drop xt-drop-static">
+            <div class="xt-card w-full shadow-drop ${classes.cardWhite()}">
+              <button type="button" class="xt-button xt-dismiss absolute top-0 right-0 p-5 text-xl" aria-label="Close">
+                ${classes.iconX()}
+              </button>
+              <div class="${classes.cardMd()}">
+                <div class="xt-h4">Drop static 0</div>
                 <p>
                   <strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam
-                  suscipit, velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris
-                  augue, sagittis vitae magna eget, vehicula scelerisque elit.
+                  suscipit, velit eu tristique mollis.
                 </p>
               </div>
-              <div class="md:w-5/12">
-                <div class="${classes.cardMd()} bg-gray-300">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non feugiat lorem, nec volutpat
-                    turpis.
-                  </p>
-                </div>
-                <div class="${classes.cardMd()}">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non feugiat lorem, nec volutpat
-                    turpis.
-                  </p>
-                </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="xt-drop-item">
+          <button
+            type="button"
+            class="xt-button ${classes.buttonMd()} ${classes.buttonRadius()} ${classes.buttonPrimary()}"
+          >
+            Drop
+          </button>
+
+          <div class="xt-drop xt-drop-static">
+            <div class="xt-card w-full shadow-drop ${classes.cardWhite()}">
+              <button type="button" class="xt-button xt-dismiss absolute top-0 right-0 p-5 text-xl" aria-label="Close">
+                ${classes.iconX()}
+              </button>
+              <div class="${classes.cardMd()}">
+                <div class="xt-h4">Drop static 1</div>
+                <p>
+                  <strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam
+                  suscipit, velit eu tristique mollis.
+                </p>
               </div>
             </div>
-            <div class="xt-media-container bg-gray-600 rounded-b-md xt-ratio-25">
-              <img class="xt-media object-cover object-center" src="/img.svg" loading="lazy" alt="" />
+          </div>
+        </div>
+
+        <div class="xt-drop-item">
+          <button
+            type="button"
+            class="xt-button ${classes.buttonMd()} ${classes.buttonRadius()} ${classes.buttonPrimary()}"
+          >
+            Drop
+          </button>
+
+          <div class="xt-drop xt-drop-static">
+            <div class="xt-card w-full shadow-drop ${classes.cardWhite()}">
+              <button type="button" class="xt-button xt-dismiss absolute top-0 right-0 p-5 text-xl" aria-label="Close">
+                ${classes.iconX()}
+              </button>
+              <div class="${classes.cardMd()}">
+                <div class="xt-h4">Drop static 2</div>
+                <p>
+                  <strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam
+                  suscipit, velit eu tristique mollis.
+                </p>
+              </div>
             </div>
           </div>
         </div>
