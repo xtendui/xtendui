46ceb03a5a62a36185ff4fd5eceee8cebb795ff4
Riccardo Caroli <rcaroli@webgriffe.com>
2021-06-10 07:19:48 +0200

    chore: fix babel messages

--- babel.config.js
+++ babel.config.js
@@ -8,4 +8,6 @@ module.exports = {
       },
     ],
   ],
+  // fix https://github.com/babel/babel/issues/11622
+  plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-private-methods'],
 }
