a67a2de2696031907fdcdf84bb274088e66012dd
Riccardo Caroli <rcaroli@webgriffe.com>
2021-06-23 17:04:53 +0200

    fix(scroll): automatic fix Xt mount and unmount on ScrollTrigger pin with raf, no need to add .xt-ignore
    This reverts commit 387651c2b9e804a40f9bbfe569ce76bf835c670c.

--- src/xt.js
+++ src/xt.js
@@ -83,13 +83,27 @@ if (typeof window !== 'undefined') {
         // added
         for (const added of mutation.addedNodes) {
           if (added.nodeType === 1) {
-            Xt.mountCheck(added)
+            cancelAnimationFrame(Xt.dataStorage.get(added, 'xtObserverFrame'))
+            Xt.dataStorage.set(
+              added,
+              'xtObserverFrame',
+              requestAnimationFrame(() => {
+                Xt.mountCheck(added)
+              })
+            )
           }
         }
         // removed
         for (const removed of mutation.removedNodes) {
           if (removed.nodeType === 1) {
-            Xt.unmountCheck(removed)
+            cancelAnimationFrame(Xt.dataStorage.get(removed, 'xtObserverFrame'))
+            Xt.dataStorage.set(
+              removed,
+              'xtObserverFrame',
+              requestAnimationFrame(() => {
+                Xt.unmountCheck(removed)
+              })
+            )
           }
         }
       }
