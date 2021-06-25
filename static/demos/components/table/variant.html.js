5cc079e672c35ecac64ad66409314105ce57ef9b
minimit <beaver82@gmail.com>
2021-06-25 18:47:03 +0200

    docs: table variant with only border top and bottom

--- static/demos/components/table/variant.html.js
+++ static/demos/components/table/variant.html.js
@@ -4,6 +4,74 @@ const html = /* HTML */ `
       <thead>
         <tr>
           <th
+            class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-xs align-top font-semibold leading-snug tracking-wider uppercase text-left"
+          >
+            #
+          </th>
+          <th
+            class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-xs align-top font-semibold leading-snug tracking-wider uppercase text-left"
+          >
+            Description
+          </th>
+          <th
+            class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-xs align-top font-semibold leading-snug tracking-wider uppercase text-left"
+          >
+            Value
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th
+            class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-xs align-top font-semibold leading-snug tracking-wider uppercase text-left *** border-t border-b ***"
+          >
+            1
+          </th>
+          <td class="py-2 px-4 text-sm align-top leading-snug *** border-t border-b ***">
+            <strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam suscipit,
+            velit eu tristique mollis, dui felis dictum turpis, a auctor est odio ac diam. Sed mauris augue, sagittis
+            vitae magna eget, vehicula scelerisque elit.
+          </td>
+          <td class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-sm align-top leading-snug *** border-t border-b ***">1200</td>
+        </tr>
+        <tr>
+          <th
+            class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-xs align-top font-semibold leading-snug tracking-wider uppercase text-left *** border-t border-b ***"
+          >
+            2
+          </th>
+          <td class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-sm align-top leading-snug *** border-t border-b ***">
+            Dolor sit amet
+          </td>
+          <td class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-sm align-top leading-snug *** border-t border-b ***">200</td>
+        </tr>
+        <tr>
+          <th
+            class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-xs align-top font-semibold leading-snug tracking-wider uppercase text-left *** border-t border-b ***"
+          >
+            3
+          </th>
+          <td class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-sm align-top leading-snug *** border-t border-b ***">
+            Consectetur adipiscing elit
+          </td>
+          <td class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-sm align-top leading-snug *** border-t border-b ***">2400</td>
+        </tr>
+        <tr>
+          <th
+            class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-xs align-top font-semibold leading-snug tracking-wider uppercase text-left *** border-t border-b ***"
+          >
+            4
+          </th>
+          <td class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-sm align-top leading-snug *** border-t border-b ***">Lorem ipsum</td>
+          <td class="py-2 px-4 *** first:pl-0 last:pr-0 *** text-sm align-top leading-snug *** border-t border-b ***">400</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <table class="my-4 xt-my-auto w-full">
+      <thead>
+        <tr>
+          <th
             class="py-2 px-4 text-xs align-top font-semibold leading-snug tracking-wider uppercase text-left *** border-2 border-primary-500 bg-primary-500 text-white ***"
           >
             #
