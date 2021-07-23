e45a09ab42985b1f182f3a865b521ce20af4b2c0
Riccardo Caroli <rcaroli@webgriffe.com>
2021-07-23 11:51:22 +0200

    docs: media skeleton

--- static/demos/components/media/variant.html.js
+++ static/demos/components/media/variant.html.js
@@ -5,11 +5,11 @@ const html = /* HTML */ `
 
       <div class="xt-media-container *** bg-gray-500 *** xt-ratio-25"></div>
 
-      <div class="xt-media-container *** !bg-gray-200 media-container--skeleton *** xt-ratio-25">
+      <div class="xt-media-container *** bg-gray-200 media-container--skeleton *** xt-ratio-25">
         <img class="xt-media h-10 w-auto absolute inset-0 m-auto" src="/logo-white.svg" loading="lazy" alt="" />
       </div>
 
-      <div class="xt-media-container *** !bg-gray-500 media-container--skeleton *** xt-ratio-25">
+      <div class="xt-media-container *** bg-gray-500 media-container--skeleton *** xt-ratio-25">
         <img class="xt-media h-10 w-auto absolute inset-0 m-auto" src="/logo-white.svg" loading="lazy" alt="" />
       </div>
     </div>
