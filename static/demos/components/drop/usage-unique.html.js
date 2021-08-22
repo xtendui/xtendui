5c6ca84728ccdd372346082d2b575550451eeb16
minimit <beaver82@gmail.com>
2021-08-22 23:59:21 +0200

    docs: demos unique

--- static/demos/components/drop/usage-unique.html.js
+++ static/demos/components/drop/usage-unique.html.js
@@ -2,54 +2,47 @@ const classes = require('src/gatsby/templates/snippets/classes').classes
 
 const html = /* HTML */ `
   <div>
-    <button
-      type="button"
-      class="xt-button ${classes.buttonMd()} rounded${classes.buttonRadius()} ${classes.buttonUpper()} ${classes.buttonPrimary()} ${classes.buttonPrimaryAnim()}"
-      data-xt-drop="{ targets: '#drop--unique-1, #drop--unique-2' }"
-    >
-      Drop
-    </button>
-
-    <div class="*** xt-drop *** p-4" id="drop--unique-1">
-      <div
-        class="xt-card w-64 rounded${classes.dropRadius()} ${classes.dropShadow()} ${classes.textBlack()} ${classes.cardGray()}"
+    <div class="xt-list xt-list-3 items-center">
+      <button
+        type="button"
+        class="xt-button ${classes.buttonMd()} rounded${classes.buttonRadius()} ${classes.buttonUpper()} ${classes.buttonPrimary()} ${classes.buttonPrimaryAnim()}"
+        data-xt-drop="{ targets: '#drop--unique' }"
       >
-        <nav class="xt-list flex-col ${classes.groupMd()}">
-          <a
-            href="#"
-            class="xt-button ${classes.groupButtonMd()} rounded${classes.groupButtonRadius()} ${classes.groupButton()} ${classes.groupButtonGray()} ${classes.buttonGrayPrimaryAnim()}"
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          </a>
-          <button
-            type="button"
-            class="xt-button ${classes.groupButtonMd()} rounded${classes.groupButtonRadius()} ${classes.groupButton()} ${classes.groupButtonGray()} ${classes.buttonGrayPrimaryAnim()}"
-          >
-            Dolor sit
-          </button>
-          <button
-            type="button"
-            class="xt-button ${classes.groupButtonMd()} rounded${classes.groupButtonRadius()} ${classes.groupButton()} ${classes.groupButtonGray()} ${classes.buttonGrayPrimaryAnim()}"
-          >
-            Amet
-          </button>
-        </nav>
-      </div>
-    </div>
+        Drop
+      </button>
 
-    <div class="*** xt-drop *** p-4" id="drop--unique-2" data-xt-position="right-end">
-      <div
-        class="xt-card w-64 rounded${classes.dropRadius()} ${classes.dropShadow()} ${classes.textBlack()} ${classes.cardGray()}"
+      <button
+        type="button"
+        class="xt-button ${classes.buttonMd()} rounded${classes.buttonRadius()} ${classes.buttonUpper()} ${classes.buttonPrimary()} ${classes.buttonPrimaryAnim()}"
+        data-xt-drop="{ targets: '#drop--unique' }"
       >
-        <button type="button" class="xt-button xt-dismiss absolute top-0 right-0 p-5 text-xl" aria-label="Close">
-          ${classes.iconX()}
-        </button>
-        <div class="${classes.cardSm()}">
-          <div class="xt-h5">Lorem ipsum</div>
-          <p>
-            <strong>Lorem ipsum</strong> dolor sit amet, <a href="#">consectetur adipiscing</a> elit. Nullam suscipit,
-            velit eu tristique mollis.
-          </p>
+        Drop Same
+      </button>
+
+      <div class="*** xt-drop *** p-4" id="drop--unique">
+        <div
+          class="xt-card w-64 rounded${classes.dropRadius()} ${classes.dropShadow()} ${classes.textBlack()} ${classes.cardGray()}"
+        >
+          <nav class="xt-list flex-col ${classes.groupMd()}">
+            <a
+              href="#"
+              class="xt-button ${classes.groupButtonMd()} rounded${classes.groupButtonRadius()} ${classes.groupButton()} ${classes.groupButtonGray()} ${classes.buttonGrayPrimaryAnim()}"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            </a>
+            <button
+              type="button"
+              class="xt-button ${classes.groupButtonMd()} rounded${classes.groupButtonRadius()} ${classes.groupButton()} ${classes.groupButtonGray()} ${classes.buttonGrayPrimaryAnim()}"
+            >
+              Dolor sit
+            </button>
+            <button
+              type="button"
+              class="xt-button ${classes.groupButtonMd()} rounded${classes.groupButtonRadius()} ${classes.groupButton()} ${classes.groupButtonGray()} ${classes.buttonGrayPrimaryAnim()}"
+            >
+              Amet
+            </button>
+          </nav>
         </div>
       </div>
     </div>
--- static/demos/components/tooltip/usage-unique.html.js
+++ static/demos/components/tooltip/usage-unique.html.js
@@ -2,21 +2,13 @@ const classes = require('src/gatsby/templates/snippets/classes').classes
 
 const html = /* HTML */ `
   <div>
-    Lorem ipsum <a href="#" data-xt-tooltip="{ targets: '#tooltip--unique-1, #tooltip--unique-2' }">dolor</a> sit amet
+    Lorem ipsum <a href="#" data-xt-tooltip="{ targets: '#tooltip--unique' }">dolor</a> sit <a href="#" data-xt-tooltip="{ targets: '#tooltip--unique' }">amet</a>
 
-    <div class="*** xt-tooltip *** p-2" id="tooltip--unique-1">
+    <div class="*** xt-tooltip *** p-2" id="tooltip--unique">
       <div
         class="relative ${classes.tooltipMd()} rounded${classes.tooltipRadius()} ${classes.tooltipShadow()} ${classes.textWhite()} ${classes.cardBlack()}"
       >
         Lorem ipsum dolor sit amet
-      </div>
-    </div>
-
-    <div class="*** xt-tooltip *** p-2" id="tooltip--unique-2" data-xt-position="right-end">
-      <div
-        class="relative ${classes.tooltipMd()} rounded${classes.tooltipRadius()} ${classes.tooltipShadow()} ${classes.textWhite()} ${classes.cardBlack()}"
-      >
-        Consectetur adipiscing elit
       </div>
     </div>
   </div>
