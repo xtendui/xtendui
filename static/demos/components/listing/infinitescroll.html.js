c64e31914dbb4dd360f6f06628fb2d8e89bfbf06
Riccardo Caroli <rcaroli@webgriffe.com>
2021-08-26 14:22:47 +0200

    chore

--- static/demos/components/listing/infinitescroll.html.js
+++ static/demos/components/listing/infinitescroll.html.js
@@ -132,6 +132,54 @@ const html = /* HTML */ `
               </div>
             </a>
           </div>
+
+          <div class="w-6/12 md:w-4/12 lg:w-3/12">
+            <a href="https://github.com/xtendui/xtendui" class="listing-item block">
+              <div class="xt-media-container bg-gray-300 pb-[75%]">
+                <img class="xt-media object-cover object-center" src="/img.svg" loading="lazy" alt="" />
+              </div>
+              <div class="py-4">
+                <div class="xt-h6">Lorem Ipsum</div>
+                <div class="-mt-2 text-sm font-medium">3.772,00 €</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="w-6/12 md:w-4/12 lg:w-3/12">
+            <a href="https://github.com/xtendui/xtendui" class="listing-item block">
+              <div class="xt-media-container bg-gray-300 pb-[75%]">
+                <img class="xt-media object-cover object-center" src="/img-alt.svg" loading="lazy" alt="" />
+              </div>
+              <div class="py-4">
+                <div class="xt-h6">Dolor sit amet</div>
+                <div class="-mt-2 text-sm font-medium">3.772,00 €</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="w-6/12 md:w-4/12 lg:w-3/12">
+            <a href="https://github.com/xtendui/xtendui" class="listing-item block">
+              <div class="xt-media-container bg-gray-300 pb-[75%]">
+                <img class="xt-media object-cover object-center" src="/img.svg" loading="lazy" alt="" />
+              </div>
+              <div class="py-4">
+                <div class="xt-h6">Lorem Ipsum</div>
+                <div class="-mt-2 text-sm font-medium">3.772,00 €</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="w-6/12 md:w-4/12 lg:w-3/12">
+            <a href="https://github.com/xtendui/xtendui" class="listing-item block">
+              <div class="xt-media-container bg-gray-300 pb-[75%]">
+                <img class="xt-media object-cover object-center" src="/img-alt.svg" loading="lazy" alt="" />
+              </div>
+              <div class="py-4">
+                <div class="xt-h6">Dolor sit amet</div>
+                <div class="-mt-2 text-sm font-medium">3.772,00 €</div>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
 
