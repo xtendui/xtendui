56a127cbf7d91ab08d2c820c062b043a501e6f24
Riccardo Caroli <rcaroli@webgriffe.com>
2021-11-12 17:33:28 +0100

    feat(test): cypress install

---
+++ tests/cypress/integration/sample_spec.js
@@ -0,0 +1,29 @@
+/* global cy */
+
+describe('My First Test visit', () => {
+  it('Visits test', () => {
+    cy.visit('/hidden/test')
+  })
+})
+
+/*
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
+
+describe('My First Test visit', () => {
+  it('finds the content "type"', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click().debug()
+
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it and verify that the value has been updated
+    cy.get('.action-email').type('fake@email.com').should('have.value', 'fake@email.com')
+  })
+})
+*/
