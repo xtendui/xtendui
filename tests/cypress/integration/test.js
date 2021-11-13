ce1098c5a5709e7d74b49567cbf4bdc6124c2482
Riccardo Caroli <rcaroli@webgriffe.com>
2021-11-13 14:50:31 +0100

    chore: refactor first test

--- tests/cypress/integration/test.js
+++ tests/cypress/integration/test.js
@@ -1,18 +1,15 @@
-describe('demos/hidden/test/mount-unmount', () => {
-  beforeEach(() => {
+describe('demos/hidden/test/mount-unmount', function () {
+  beforeEach(function () {
     cy.visit('/hidden/test')
+    cy.window().as('win')
+    cy.get('.demo--mount-unmount').as('demo')
+    cy.get('@demo').find('[data-xt-overlay]').as('overlay')
   })
-
-  it('TEST INITIAL xtNamespace should be 1.', () => {
-    cy.window()
-      .its('Xt')
-      .then(Xt => {
-        const demo = cy.get('.demo--mount-unmount')
-        demo.find('[data-xt-overlay]').then(overlay => {
-          const self = Xt.get({ name: 'xt-overlay', el: overlay[0] })
-          const xtNamespace = Xt.dataStorage.get(self.ns, 'xtNamespace')
-          expect(xtNamespace.length).to.equal(1)
-        })
-      })
+  it('TEST INITIAL xtNamespace should be 1.', function () {
+    const win = this.win
+    const overlay = this.overlay[0]
+    const self = win.Xt.get({ name: 'xt-overlay', el: overlay })
+    const xtNamespace = win.Xt.dataStorage.get(self.ns, 'xtNamespace')
+    expect(xtNamespace.length).to.equal(1)
   })
 })
