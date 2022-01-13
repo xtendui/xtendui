85645e3b326750bfe29f78cbd40ae8e810c97ffb
Riccardo Caroli <rcaroli@webgriffe.com>
2022-01-13 20:05:08 +0100

    chore: removed gatsby prismic and refactor gatsby not esm

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,98 @@
-module.exports = require('esm')(module)('./gatsby-node.esm.js')
+const path = require('path')
+const markdownSlug = require('./src/gatsby/templates/snippets/markdown-slug.js').markdownSlug
+
+// webpack config
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        // resolve xtendui import js and css
+        xtendui: path.resolve(__dirname, './'),
+      },
+    },
+  })
+}
+
+exports.onCreateBabelConfig = ({ actions }) => {
+  actions.setBabelPreset({
+    name: `babel-preset-gatsby`, // https://github.com/gatsbyjs/gatsby/blob/master/packages/babel-preset-gatsby/README.md
+  })
+  actions.setBabelPreset({
+    name: `@babel/preset-env`,
+    options: {
+      useBuiltIns: 'entry',
+      corejs: 3,
+    },
+  })
+}
+
+// markdown
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+  const docPageTemplate = path.resolve('src/gatsby/templates/doc-page.js')
+  const docCategoryTemplate = path.resolve('src/gatsby/templates/doc-category.js')
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              type
+              category
+              parent
+              title
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      const slug = markdownSlug(node)
+      createPage({
+        path: slug,
+        component: node.frontmatter.parent ? docPageTemplate : docCategoryTemplate,
+        context: {
+          // for graphql query($type: String)
+          title: node.frontmatter.title,
+          type: node.frontmatter.type,
+          category: node.frontmatter.category,
+          parent: node.frontmatter.parent,
+          parents: `/^${node.frontmatter.parent}$/`,
+        },
+      })
+    })
+  })
+}
+
+// contenful
+if (
+  process.env.NODE_ENV === 'development' ||
+  !process.env.CONTENTFUL_SPACE_ID ||
+  !process.env.CONTENTFUL_ACCESS_TOKEN
+) {
+  // contentful dummy content https://www.gatsbyjs.com/docs/recipes/sourcing-data/
+  exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+    const contentfulAssets = [
+      { title: 'dummy-asset', localFile: { publicURL: 'dummy-url' }, file: { url: 'dummy-url' } },
+    ]
+    contentfulAssets.forEach(contentfulAsset => {
+      const node = {
+        title: contentfulAsset.title,
+        file: contentfulAsset.file,
+        localFile: contentfulAsset.localFile,
+        id: createNodeId(`ContentfulAsset-${contentfulAsset.name}`),
+        internal: {
+          type: 'ContentfulAsset',
+          contentDigest: createContentDigest(contentfulAsset),
+        },
+      }
+      actions.createNode(node)
+    })
+  }
+}
--- src/gatsby/templates/snippets/markdown-slug.js
+++ src/gatsby/templates/snippets/markdown-slug.js
@@ -1,18 +1,20 @@
-import kebabCase from 'lodash/kebabcase'
+const kebabCase = require('lodash/kebabcase')
 
-export const markdownSlug = page => {
-  const slugType = kebabCase(page.frontmatter.type)
-  const slugParent = kebabCase(page.frontmatter.parent)
-  const slugTitle = kebabCase(page.frontmatter.title)
-  let slug = ''
-  if (page.frontmatter.type) {
-    slug += `/${slugType}`
-  }
-  if (page.frontmatter.parent) {
-    slug += `/${slugParent}`
-  }
-  if (slugTitle !== slugType && slugTitle !== slugParent) {
-    slug += `/${slugTitle}`
-  }
-  return slug
+module.exports = {
+  markdownSlug: page => {
+    const slugType = kebabCase(page.frontmatter.type)
+    const slugParent = kebabCase(page.frontmatter.parent)
+    const slugTitle = kebabCase(page.frontmatter.title)
+    let slug = ''
+    if (page.frontmatter.type) {
+      slug += `/${slugType}`
+    }
+    if (page.frontmatter.parent) {
+      slug += `/${slugParent}`
+    }
+    if (slugTitle !== slugType && slugTitle !== slugParent) {
+      slug += `/${slugTitle}`
+    }
+    return slug
+  },
 }
