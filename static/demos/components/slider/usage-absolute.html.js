5b2c6cf4b6142edcb6229b2b1189d5d76ad0e3ca
Riccardo Caroli <rcaroli@webgriffe.com>
2022-09-12 15:36:09 +0200

    docs: fix slider absolute wrong divs

--- static/demos/components/slider/usage-absolute.html.js
+++ static/demos/components/slider/usage-absolute.html.js
@@ -51,18 +51,18 @@ const html = /* HTML */ `
             </div>
           </div>
         </div>
-      </div>
 
-      <div class="*** xt-slide xt-slide-absolute off:hidden *** w-full" data-xt-slider-target>
-        <div class="hero relative overflow-hidden bg-black">
-          <div class="hero-inner">
-            <div class="xt-media-container bg-gray-200 w-full h-full absolute">
-              <img class="xt-media object-cover" src="/img-alt.svg" loading="lazy" alt="" />
-            </div>
-            <div class="flex relative h-96">
-              <div
-                class="relative px-8 py-16 md:px-24 lg:py-20 xl:py-24 mt-auto mx-auto *** max-w-2xl *** ${classes.textInverse()} text-center">
-                <h2 class="xt-h1 font-black text-4xl md:text-5xl lg:text-6xl">4</h2>
+        <div class="*** xt-slide xt-slide-absolute off:hidden *** w-full" data-xt-slider-target>
+          <div class="hero relative overflow-hidden bg-black">
+            <div class="hero-inner">
+              <div class="xt-media-container bg-gray-200 w-full h-full absolute">
+                <img class="xt-media object-cover" src="/img-alt.svg" loading="lazy" alt="" />
+              </div>
+              <div class="flex relative h-96">
+                <div
+                  class="relative px-8 py-16 md:px-24 lg:py-20 xl:py-24 mt-auto mx-auto *** max-w-2xl *** ${classes.textInverse()} text-center">
+                  <h2 class="xt-h1 font-black text-4xl md:text-5xl lg:text-6xl">4</h2>
+                </div>
               </div>
             </div>
           </div>
