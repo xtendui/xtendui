4c77f26edd5cbf35ac2b457f3d86156755e0e00b
Riccardo Caroli <rcaroli@webgriffe.com>
2022-12-06 15:41:51 +0100

    docs: small refactor demos

--- src/gatsby/templates/demo/demo-inline.js
+++ src/gatsby/templates/demo/demo-inline.js
@@ -7,8 +7,6 @@ function DemoInline(props) {
   const id = src.split('-').join(' ')
   const name = src.split('/').pop().split('.')[0]
   // vanilla
-  const object = require(`static/${src}.html.js`).object
-  const html = object.html
   let hasCss
   let hasJs
   try {
@@ -27,10 +25,26 @@ function DemoInline(props) {
   } catch (ex) {}
   // react
   const Demo = require(`static/${src}.jsx`).default
+  /* @TODO lazy
+  const Demo = loadable(() => import('static/demos/components/toggle/animation-noqueue.jsx'))
+  */
   // mode
   const [mode, setMode] = useState(0)
+  const [object, setObject] = useState(0)
   const ref = useRef()
   useEffect(() => {
+    // html
+    setObject(require(`static/${src}.html.js`).object)
+    /* @TODO lazy
+    loadable(
+      import('static/demos/components/toggle/animation-noqueue.html.js').then(module => {
+        setObject(module.object)
+      })
+    )
+    */
+  })
+  useEffect(() => {
+    // switch demo
     const item = ref.current
     const switchDemo = mode => {
       // needs raf or useLayout inside demos is executed before mutation observer Xt._mountCheck({ added })
@@ -80,9 +94,9 @@ function DemoInline(props) {
             className={`gatsby_demo_source gatsby_demo_source--from gatsby_demo_source--container ${
               object.overflow ? 'gatsby_demo_source--overflow' : ''
             }`}
-            dangerouslySetInnerHTML={{ __html: html }}
+            dangerouslySetInnerHTML={{ __html: object.html }}
           />
-          <script type="text/plain" data-lang="html" dangerouslySetInnerHTML={{ __html: html }} />
+          <script type="text/plain" data-lang="html" dangerouslySetInnerHTML={{ __html: object.html }} />
           {hasCss ? (
             <script
               type="text/plain"
