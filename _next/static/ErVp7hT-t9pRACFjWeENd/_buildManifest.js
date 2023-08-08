2168ea2c8c2899a8d1d37c9448aef9ba93ff69ae
Riccardo Caroli <beaver82@gmail.com>
2023-08-08 10:48:08 +0200

    Updates

---
+++ _next/static/ErVp7hT-t9pRACFjWeENd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-9aeb3e4d490fe4b8.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/chunks/app/not-found-756a96df512be5c2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{63521:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
