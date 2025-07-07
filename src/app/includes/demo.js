b81139f834c9bdcd72547e6e1c208facb21a1787
Riccardo Caroli <beaver82@gmail.com>
2025-07-07 12:54:21 +0200

    chore

--- src/app/includes/demo.js
+++ src/app/includes/demo.js
@@ -18,7 +18,7 @@ class Demo extends Component {
   render() {
     const { children } = this.props
     return (
-      <div>
+      <div className="docs_demo">
         <div className="docs_demo" ref={this.demoRef}>
           <div className="docs_demo_inner">{children}</div>
         </div>
