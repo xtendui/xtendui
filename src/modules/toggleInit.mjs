069d02c1ce5f9e8de838d3b11587d075f9b06516
Riccardo Caroli <beaver82@gmail.com>
2025-07-18 10:53:53 +0200

    fix(js): fix appendTo do not move if the parent element is already the appendTo element

--- src/modules/toggleInit.mjs
+++ src/modules/toggleInit.mjs
@@ -2538,18 +2538,20 @@ export class ToggleInit {
         if (actionCurrent === 'In') {
           // appendTo
           const appendToTarget = document.querySelector(options.appendTo)
+          const appendOrigin = document.querySelector(`[data-xt-origin="${self.ns}"]`)
+          if (!appendOrigin) {
+            el.before(Xt.node({ str: `<div class="xt-ignore hidden" data-xt-origin="${self.ns}"></div>` }))
+          }
           if (el.parentElement !== appendToTarget) {
-            const appendOrigin = document.querySelector(`[data-xt-origin="${self.ns}"]`)
-            if (!appendOrigin) {
-              el.before(Xt.node({ str: `<div class="xt-ignore hidden" data-xt-origin="${self.ns}"></div>` }))
-            }
             appendToTarget.append(el)
           }
         } else if (actionCurrent === 'Out') {
           // appendTo
           const appendOrigin = document.querySelector(`[data-xt-origin="${self.ns}"]`)
           if (appendOrigin) {
-            appendOrigin.before(el)
+            if (el.parentElement !== appendOrigin.parentElement) {
+              appendOrigin.before(el)
+            }
             appendOrigin.remove()
           } else {
             el.remove()
