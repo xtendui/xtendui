2d7cbdd5970ffb8b6deebd7cd163ebb943983913
Riccardo Caroli <beaver82@gmail.com>
2026-03-04 16:48:06 +0100

    chore: better llmtxt

--- util/packet.js
+++ util/packet.js
@@ -1,4 +1,5 @@
 const { glob } = require('glob')
+const fs = require('fs')
 const writeFile = require('write')
 
 async function compileCss() {
@@ -21,24 +22,26 @@ async function compileJs() {
 }
 compileJs()
 
-async function compileLLM() {
-  let str = `# [Xtendui](https://xtendui.github.io/xtendui/)
-
-Xtend UI is a powerful frontend library of Tailwind CSS components enhanced by vanilla js. It helps you build interfaces with advanced interactions and animations.
-
-- [intro](https://raw.githubusercontent.com/xtendui/xtendui/refs/heads/master/README.md)
-- [setup](https://raw.githubusercontent.com/xtendui/xtendui/refs/heads/master/SETUP.md)
-- [features](https://raw.githubusercontent.com/xtendui/xtendui/refs/heads/master/src/app/markdown/intro/features/index.md)
+const intro = `# [Xtendui](https://xtendui.github.io/xtendui/)
 
 ## Demos Source Code Extraction
 
 In the [website](https://xtendui.github.io/xtendui/), demos have source code generated in html:
 - **Location:** '<script type="text/plain" class="docs_demo_source">'
 - **Language:** Indicated by 'data-lang' (html, css, js).
-- **External source code:** If 'data-fetch' is present, the full source is at 'https://xtendui.github.io' + the attribute value.
+- **External source code:** If 'data-fetch' is present in the website source, the full source is at 'https://xtendui.github.io' + the attribute value.
+- **Demos in .md files:** If 'demos: []' is present, the full demos is at 'https://xtendui.github.io/xtendui/demos/' + the demo string inside demos array.\n`
 
-## Documentation\n`
-  const files = await glob('src/app/markdown/**/*.md', { ignore: ['src/**/hidden/**'] })
+async function compileLLM() {
+  let str = intro
+  str += `\n# Intro\n\n`
+  const filesIntro = await glob(['README.md', 'src/app/markdown/**/intro/**/*.md'], { ignore: ['src/**/hidden/**'] })
+  for (const file of filesIntro.sort()) {
+    const cleanLabel = file.replace('src/app/markdown/', '').replace('.md', '').replace('/index', '')
+    str += `- [${cleanLabel}](https://raw.githubusercontent.com/xtendui/xtendui/refs/heads/master/${file})\n`
+  }
+  str += `\n# Docs\n\n`
+  const files = await glob('src/app/markdown/**/*.md', { ignore: ['src/**/hidden/**', 'src/**/intro/**'] })
   for (const file of files.sort()) {
     const cleanLabel = file.replace('src/app/markdown/', '').replace('.md', '').replace('/index', '')
     str += `- [${cleanLabel}](https://raw.githubusercontent.com/xtendui/xtendui/refs/heads/master/${file})\n`
@@ -46,3 +49,21 @@ In the [website](https://xtendui.github.io/xtendui/), demos have source code gen
   writeFile('./llms.txt', str)
 }
 compileLLM()
+
+async function compileLLMFull() {
+  let str = intro
+  const filesIntro = await glob(['README.md', 'src/app/markdown/**/intro/**/*.md'], { ignore: ['src/**/hidden/**'] })
+  for (const file of filesIntro.sort()) {
+    const cleanLabel = file.replace('src/app/markdown/', '').replace('.md', '').replace('/index', '')
+    str += `\n# ${cleanLabel}\n\n`
+    str += `${fs.readFileSync(file, 'utf8')}`
+  }
+  const files = await glob('src/app/markdown/**/*.md', { ignore: ['src/**/hidden/**', 'src/**/intro/**'] })
+  for (const file of files.sort()) {
+    const cleanLabel = file.replace('src/app/markdown/', '').replace('.md', '').replace('/index', '')
+    str += `\n# ${cleanLabel}\n\n`
+    str += `${fs.readFileSync(file, 'utf8')}`
+  }
+  writeFile('./llms-full.txt', str)
+}
+compileLLMFull()
